57e7894a919fd66d9471a1d58d9d9b4d05a99092
Martin Spott <Martin.Spott@mgras.net>
2011-11-25 07:05:46 -0800

    Add Copyright

--- WWW/mapserver/map/index.php
+++ WWW/mapserver/map/index.php
@@ -1,6 +1,25 @@
+<!--
+  FlightGear MapServer Landcover-DB OpenLayers Map
+  Copyright (C) 200x - 2011  Martin Spott - Martin (at) flightgear (dot) org
+ 
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of the
+  License, or (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+
 <HTML>
   <HEAD>
-    <TITLE>FlightGear MapServer OpenLayers Demo</TITLE>
+    <TITLE>FlightGear Land Web Map</TITLE>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta name="robots" content="index, nofollow" />
     <meta name="keywords" content="VMap0, VMap1, GSHHS, PGS, SWBD, DAFIF, ICAO, PostgreSQL, PostGIS, Mapserver, OGC, FlightGear, OSGeo, TelaScience" />
