9444b811b999f31756a855a217551ec14c696ba4
Martin Spott <Martin.Spott@mgras.net>
2012-01-04 00:53:11 -0800

    Make Landcover-DB download scripts public. Rather ugly but working. Maybe somebody wants to supply some care by merging the various copies into fewer more generic files.

---
+++ WWW/mapserver/dlaction.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+#
+# called by 'dlaction.psp'
+#
+# Supply bounding box for 'pgsql2shp' as:
+#   <xmin> <ymin>,<xmax> <ymax>
+
+UUID=${1}
+PGHOST=geoscope.optiputer.net
+PGUSER=webuser
+PGDATABASE=landcover
+PSQL="psql -h ${PGHOST} -d ${PGDATABASE} -U webuser -tA"
+BASEDIR=/home/martin
+PGSQL2SHP=/opt/PostgreSQL/bin/pgsql2shp
+DUMPDIR=${BASEDIR}/SHPdump/${UUID}
+DLDIR=${BASEDIR}/SHPdl
+
+GeomSelect() {
+  ${PSQL} -c "SELECT ST_AsText(${1}_geometry) FROM download \
+    WHERE uuid LIKE '${UUID}'" | cut -f 2 -d \( | cut -f 1 -d \)
+}
+
+HasMapLayer() {
+  ${PSQL} -c "SELECT count(conf_layer.pgislayer) FROM conf_layer, download \
+    WHERE download.uuid LIKE '${UUID}' and download.pgislayer = conf_layer.maplayer"
+}
+
+LayerSelect() {
+  if [ `HasMapLayer` = 0 ]; then
+    ${PSQL} -c "SELECT conf_layer.pgislayer FROM conf_layer, download \
+      WHERE download.uuid LIKE '${UUID}' and download.pgislayer = conf_layer.pgislayer"
+  elif [ `HasMapLayer` = 1 ]; then
+    ${PSQL} -c "SELECT conf_layer.pgislayer FROM conf_layer, download \
+      WHERE download.uuid LIKE '${UUID}' and download.pgislayer = conf_layer.maplayer"
+  fi
+}
+
+if [ `HasMapLayer` = 1 ]; then
+  SQLFILTER="AND `${PSQL} -c "SELECT conf_layer.sqlfilter FROM conf_layer, download \
+    WHERE download.uuid LIKE '${UUID}' and download.pgislayer = conf_layer.maplayer"`"
+else
+  SQLFILTER=""
+fi
+
+FileName() {
+  ${PSQL} -c "SELECT pgislayer FROM download \
+    WHERE uuid LIKE '${UUID}'"
+}
+
+LL_GEOMETRY=`GeomSelect ll`
+UR_GEOMETRY=`GeomSelect ur`
+LAYER=`LayerSelect`
+BBOX="${LL_GEOMETRY}, ${UR_GEOMETRY}"
+
+echo ${LAYER}
+echo ${BBOX}
+HasMapLayer
+echo ${SQLFILTER}
+FileName
+
+mkdir ${DUMPDIR} && cd ${DUMPDIR}/ || exit 1
+rm -f *
+
+${PGSQL2SHP} -f ${DUMPDIR}/${LAYER}.shp \
+  -h ${PGHOST} -u ${PGUSER} -g wkb_geometry -b -r ${PGDATABASE} \
+  "SELECT * FROM ${LAYER} \
+    WHERE wkb_geometry && \
+    ST_SetSRID('BOX3D(${BBOX})'::BOX3D, 4326 ) ${SQLFILTER}"
+
+cp -a ${BASEDIR}/landcover/EPSG4326.prj ${DUMPDIR}/${LAYER}\.prj
+zip ${DLDIR}/`FileName`-${UUID}\.zip `FileName`*
+cd ${DUMPDIR}/.. && rm -rf ${UUID}
+# EOF
---
+++ WWW/mapserver/dlclc.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# called by 'dlclc.psp'
+#
+# Supply bounding box for 'pgsql2shp' as:
+#   <xmin> <ymin>,<xmax> <ymax>
+
+UUID=${1}
+PGHOST=geoscope.optiputer.net
+PGDATABASE=landcover
+PGUSER=webuser
+PSQL="psql -h ${PGHOST} -d ${PGDATABASE} -U webuser -tA -c"
+BASEDIR=/home/martin
+PGSQL2SHP=/opt/PostgreSQL/bin/pgsql2shp
+DUMPDIR=${BASEDIR}/SHPdump/${UUID}
+DLDIR=${BASEDIR}/SHPdl
+
+GeomSelect() {
+  ${PSQL} "SELECT ST_AsText(${1}_geometry) FROM download \
+    WHERE uuid LIKE '${UUID}'" | cut -f 2 -d \( | cut -f 1 -d \)
+}
+
+LL_GEOMETRY=`GeomSelect ll`
+UR_GEOMETRY=`GeomSelect ur`
+BBOX="${LL_GEOMETRY}, ${UR_GEOMETRY}"
+
+mkdir ${DUMPDIR} && cd ${DUMPDIR}/ || exit 1
+rm -f *
+
+for LAYER in `${PSQL} "SELECT f_table_name FROM geometry_columns \
+        WHERE f_table_name LIKE 'clc\_%' \
+        AND (type LIKE 'ST_Polygon' OR type LIKE 'POLYGON') \
+        ORDER BY f_table_name"`; do
+    COUNT=`${PSQL} "SELECT COUNT(wkb_geometry) FROM ${LAYER} \
+              WHERE wkb_geometry && \
+              ST_SetSRID('BOX3D(${BBOX})'::BOX3D, 4326)"`
+    if [ ${COUNT} -gt 0 ]; then
+        ${PGSQL2SHP} -f ${DUMPDIR}/${LAYER}.shp \
+            -h ${PGHOST} -u ${PGUSER} -g wkb_geometry -b -r ${PGDATABASE} \
+            "SELECT * FROM ${LAYER} \
+              WHERE wkb_geometry && \
+              ST_SetSRID('BOX3D(${BBOX})'::BOX3D, 4326)"
+        cp -a ${BASEDIR}/landcover/EPSG4326.prj ${DUMPDIR}/${LAYER}\.prj
+    fi
+done
+
+zip ${DLDIR}/${UUID}\.zip *
+cd ${DUMPDIR}/.. && rm -rf ${UUID}
+
+# EOF
