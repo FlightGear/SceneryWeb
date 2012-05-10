cf9308e1de2559736add8cf6c218d17ad8e1c4d0
clement <clemaez@hotmail.fr>
2012-05-10 12:53:25 +0200

    modelbrowser.php is now FIXED

--- WWW/scenemodels/modelbrowser.php
+++ WWW/scenemodels/modelbrowser.php
@@ -1,36 +1,29 @@
 <?php
 include 'header.php';
+$link=pg_connect('dbname='.$dbname.' host='.$dbhost.' user='.$dbuser.' password='.$dbpass.' sslmode=disable');
 
-  $link=pg_connect('dbname='.$dbname.' host='.$dbhost.' user='.$dbuser.' password='.$dbpass.' sslmode=disable');
-if (isset($_REQUEST['offset']) && (preg_match('/^[0-9]+$/u',$_GET['offset'])))
-{
+if (isset($_REQUEST['offset']) && (preg_match('/^[0-9]+$/u',$_GET['offset']))){
   $offset=$_REQUEST['offset'];
   if ($offset<0) $offset=0;      
+}else{
+  $offset=0;
 }
-else
-{       $offset=0;
-};
-if (isset($_REQUEST['shared']) && (preg_match('/^[0-9]+$/u',$_GET['shared'])))
-{       
-  if ($_REQUEST['shared']>0)
-  {
+
+if (isset($_REQUEST['shared']) && (preg_match('/^[0-9]+$/u',$_GET['shared']))){       
+  if ($_REQUEST['shared']>0){
     $groupquery="SELECT mg_name FROM fgs_modelgroups WHERE mg_id =".$_REQUEST['shared'].";";
     $gpresult=pg_query($groupquery);
     $row=pg_fetch_assoc($gpresult);
     $title="Model Browser: ".$row['mg_name'];
     $query="select mo_id,mo_path,mo_name from fgs_models where mo_shared =".$_REQUEST['shared']." order by mo_id limit 100 offset $offset;";
-  }
-  else
-  {
+  }else{
     $title="FlightGear Scenery Static Model Browser";
     $query="select mo_id,mo_path,mo_name from fgs_models where mo_shared = 0 order by mo_id limit 100 offset $offset;";
   }  
-}
-else
-{
+}else{
   $query="select mo_id,mo_path,mo_name from fgs_models order by mo_id limit 100 offset $offset;";
   $title="FlightGear Scenery Model Browser";
-};
+}
 ?>
 
 <h1 align=center><?php echo $title;?></h1>
@@ -51,14 +44,22 @@ else
         while ($row = pg_fetch_assoc($result)){
       ?>
         <a href="/modeledit.php?id=<?php echo $row['mo_id'];?>">
-
-
-<img border="0" title="<?php echo $row['mo_name'].' ['.$row['mo_path'].']';?>" src="modelthumb.php?id=<?php echo $row['mo_id'];?>" width=100 height=75 onmouseover="showtrail('modelthumb.php?id=<?php echo $row['mo_id'];?>','','','1',5,322);" onmouseout="hidetrail();"></a>
-<?php
-}
-?>
-</td></tr>
-<tr class=bottom><td colspan=9 align=center><a href="modelbrowser.php?offset=<?php echo $offset-100;if (isset($_REQUEST['shared'])) {echo "&shared=".$_REQUEST['shared'];};?>">Prev</a> <a href="modelbrowser.php?offset=<?php echo $offset+100;if (isset($_REQUEST['shared'])) {echo "&shared=".$_REQUEST['shared'];};?>">Next</a></td></tr>
+          <img border="0" title="<?php echo $row['mo_name'].' ['.$row['mo_path'].']';?>" 
+               src="modelthumb.php?id=<?php echo $row['mo_id'];?>" width="100" height="75" 
+               onmouseover="showtrail('modelthumb.php?id=<?php echo $row['mo_id'];?>','','','1',5,322);" 
+               onmouseout="hidetrail();" />
+        </a>
+      <?php
+      }
+      ?>
+    </td>
+  </tr>
+  <tr class=bottom>
+    <td colspan=9 align=center>
+      <a href="modelbrowser.php?offset=<?php echo $offset-100;if (isset($_REQUEST['shared'])) {echo "&shared=".$_REQUEST['shared'];};?>">Prev</a> 
+      <a href="modelbrowser.php?offset=<?php echo $offset+100;if (isset($_REQUEST['shared'])) {echo "&shared=".$_REQUEST['shared'];};?>">Next</a>
+    </td>
+  </tr>
 </table>
 </body>
 </html>
