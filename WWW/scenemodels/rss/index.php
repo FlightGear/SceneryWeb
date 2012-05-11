612b24448ba242522f234a7801e2cf011d2271fe
Martin Spott <Martin.Spott@mgras.net>
2012-05-11 16:04:19 +0200

    Add lost header

--- WWW/scenemodels/rss/index.php
+++ WWW/scenemodels/rss/index.php
@@ -1,17 +1,22 @@
-<?php include '../header.php';?>
-<h1 align=center>RSS Feeds</h1>
-<table>
-  <tr><th>Keeping Up To Date</th></tr>
-  <tr>
-    <td>
-      <p>To ensure you don't miss anything the following RSS feeds are available:</p>
-      <ul>
-        <li><a href="news.php">News</a> FGFSDB site news</li>
-        <li><a href="models.php">Models</a> Recent model additions</li>
-      </ul>
-      <br/><br/>
-    </td>
-  </tr>
-</table>
+<html>
+<head>
+  <link rel="stylesheet" href="/style.css" type="text/css">
+</head>
+<body>
+  <?php include '../header.php';?>
+  <h1 align=center>RSS Feeds</h1>
+  <table>
+    <tr><th>Keeping Up To Date</th></tr>
+    <tr>
+      <td>
+        <p>To ensure you don't miss anything the following RSS feeds are available:</p>
+        <ul>
+          <li><a href="news.php">News</a> FGFSDB site news</li>
+          <li><a href="models.php">Models</a> Recent model additions</li>
+        </ul>
+        <br/><br/>
+      </td>
+    </tr>
+  </table>
 </body>
 </html>
