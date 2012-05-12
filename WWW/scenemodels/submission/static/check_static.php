185b357e6f8d7556a903483208181469e0817083
clement <clemaez@hotmail.fr>
2012-05-12 19:10:49 +0200

    Move function to function.inc.php

--- WWW/scenemodels/submission/static/check_static.php
+++ WWW/scenemodels/submission/static/check_static.php
@@ -6,25 +6,6 @@ $fatalerror = 0;
 $error      = 0;
 $errormsg   = "";
 
-function clearDir($dossier) {
-  $ouverture=@opendir($dossier);
-  if (!$ouverture) return;
-  while($fichier=readdir($ouverture)) {
-    if ($fichier == '.' || $fichier == '..') continue;
-    if (is_dir($dossier."/".$fichier)) {
-      $r=clearDir($dossier."/".$fichier);
-      if (!$r) return false;
-    }else{
-      $r=@unlink($dossier."/".$fichier);
-      if (!$r) return false;
-    }
-  }
-  closedir($ouverture);
-  $r=@rmdir($dossier);
-  if (!$r) return false;
-  return true;
-}
-
 /*
 $privatekey = "6Len6skSAAAAACnlhKXCda8vzn01y6P9VbpA5iqi";
 $resp = recaptcha_check_answer ($privatekey, $_SERVER["REMOTE_ADDR"], $_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]);
