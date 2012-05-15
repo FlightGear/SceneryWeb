5d0f199f81b30905cd28d99c2e405f1cb40bcfb5
clement <clemaez@hotmail.fr>
2012-05-16 01:58:37 +0200

    Update of the scenery.flightgear.org project

--- WWW/scenemodels/scenery.flightgear.org/models-library.php
+++ WWW/scenemodels/scenery.flightgear.org/models-library.php
@@ -23,7 +23,7 @@
     $author = "";
   }
 
-  if (isset($_REQUEST['orderby']) && $_REQUEST['orderby']>0){
+  if (isset($_REQUEST['orderby']) && $_REQUEST['orderby']!=""){
     $orderby = $_REQUEST['orderby'];
     $filter.= " ORDER BY=".$_REQUEST['orderby'];
   }else{
