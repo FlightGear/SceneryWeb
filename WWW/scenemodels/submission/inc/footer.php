ea4a04f991fd7fba827b4e8b71502f6a2fe0d01c
OliverJ <acomv41@yahoo.com>
2012-06-16 10:34:25 +0200

    Will be better if centered.

--- WWW/scenemodels/submission/inc/footer.php
+++ WWW/scenemodels/submission/inc/footer.php
@@ -1,3 +1,4 @@
-Version 1.1.6 - 12/06/06 - <a href="http://scenemodels.flightgear.org/submission/VERSION">Version info</a> - <a href="http://scenemodels.flightgear.org/submission/TOBEDONE">Volunteer ?</a> - <a href="http://scenemodels.flightgear.org/submission/README">README</a> - <a href="http://scenemodels.flightgear.org/submission/LICENCE">License</a>
+<center>Version 1.1.6 - 12/06/06 - <a href="http://scenemodels.flightgear.org/submission/VERSION">Version info</a> - <a href="http://scenemodels.flightgear.org/submission/TOBEDONE">Volunteer ?</a> - <a href="http://scenemodels.flightgear.org/submission/README">README</a> - <a href="http://scenemodels.flightgear.org/submission/LICENCE">License</a>
+</center>
 </body>
 </html>
