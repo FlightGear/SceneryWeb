258c60f6cf3ec610576020c84ebce906a4c587b7
OliverJ <acomv41@yahoo.com>
2012-06-21 17:21:53 +0200

    One last update on footer.

--- WWW/scenemodels/inc/footer.php
+++ WWW/scenemodels/inc/footer.php
@@ -1,5 +1,5 @@
 <br />
-<center>Version
+<center>
 <?php
 
 // What's the last GIT version of the website and when was it last updated?
@@ -8,7 +8,7 @@ $filename = '/home/fgscenery/sceneryweb/.git/ORIG_HEAD';
 if (file_exists($filename))
 {
 $result = file_get_contents($filename);
-echo "Version".substr($result,0,7)."&nbsp;-&nbsp;" . date("F d Y H:i", filemtime($filename)) ."&nbsp;-&nbsp;";
+echo "Version&nbsp;".substr($result,0,7)."&nbsp;-&nbsp;" . date("F d Y H:i", filemtime($filename)) ."&nbsp;-&nbsp;";
 }
 
 ?>
