3e8c122d8d667c5b4229a14cd54adc68559ddd3a
OliverJ <acomv41@yahoo.com>
2012-06-21 14:50:35 +0200

    Moved the inc upper and put the header file in it, will make it easier to maintain.

---
+++ WWW/scenemodels/inc/footer.php
@@ -0,0 +1,4 @@
+<center>Version 1.1.6 - 12/06/06 - <a href="../../VERSION">Version info</a> - <a href="../../TOBEDONE">Volunteer ?</a> - <a href="../../README">README</a> - <a href="../../LICENCE">License</a>
+</center>
+</body>
+</html>
