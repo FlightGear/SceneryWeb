f51010d42172d902aa15b3fbe358829c81e102ba
Martin Spott <Martin.Spott@mgras.net>
2012-06-22 10:58:22 +0200

    Change directory name.

--- WWW/scenemodels/inc/footer.php
+++ WWW/scenemodels/inc/footer.php
@@ -4,7 +4,7 @@
 
 // What's the last GIT version of the website and when was it last updated?
 
-$filename = '/home/fgscenery/sceneryweb/.git/ORIG_HEAD';
+$filename = '/home/fgscenery/WWW/.git/ORIG_HEAD';
 if (file_exists($filename))
 {
 $result = file_get_contents($filename);
@@ -12,7 +12,7 @@ echo "Version&nbsp;".substr($result,0,7)."&nbsp;-&nbsp;" . date("F d Y H:i", fil
 }
 
 ?>
-<a href="https://www.gitorious.org/fg/sceneryweb/commits/master">Version info</a> - <a href="../../TOBEDONE">Volunteer ?</a> - <a href="../../README">README</a> - <a href="../../LICENCE">License</a> - <a href="../../VERSION">History</a>
+<a href="https://www.gitorious.org/fg/WWW/commits/master">Version info</a> - <a href="../../TOBEDONE">Volunteer ?</a> - <a href="../../README">README</a> - <a href="../../LICENCE">License</a> - <a href="../../VERSION">History</a>
 </center>
 </body>
 </html>
