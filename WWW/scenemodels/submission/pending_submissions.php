619819e1b9243cfa1b46c38538ac2dc3e753b7e5
Julien Nguyen <jnguyen@etu.emse.fr>
2012-06-26 11:29:33 +0200

    Small indentation issue due to tabs fixed

--- WWW/scenemodels/submission/pending_submissions.php
+++ WWW/scenemodels/submission/pending_submissions.php
@@ -17,74 +17,74 @@ $resultr = @pg_query($resource_r, $pending_queries);
 
 if($resultr)
 {
-	// Declare variables
-	$i = 0; 
-	$pending_requests = "";
+    // Declare variables
+    $i = 0; 
+    $pending_requests = "";
 
-	// Retrieving information
+    // Retrieving information
 
-	while($row = @pg_fetch_object($resultr))
-	{
+    while($row = @pg_fetch_object($resultr))
+    {
         $i++;
-	
-	// Decoding in Base64.
-	
-	$base64_decoded_query = base64_decode($row->spr_base64_sqlz);
+        
+        // Decoding in Base64.
+        
+        $base64_decoded_query = base64_decode($row->spr_base64_sqlz);
 
-	// Dezipping the Base64'd request.
-	
-	$unzipped_base64_query = gzuncompress($base64_decoded_query);
-	
+        // Dezipping the Base64'd request.
+        
+        $unzipped_base64_query = gzuncompress($base64_decoded_query);
+        
         $pending_requests .= "\nRequest #".$i." identified by ".$row->spr_hash."\n";
-	$pending_requests .= "=========================================================================================\n";
-	$pending_requests .= $unzipped_base64_query."\n";	
-	}
-	
-	// Sets the time to UTC.
+        $pending_requests .= "=========================================================================================\n";
+        $pending_requests .= $unzipped_base64_query."\n";
+    }
+    
+    // Sets the time to UTC.
 
-	date_default_timezone_set('UTC');
-	$dtg = date('l jS \of F Y h:i:s A');
+    date_default_timezone_set('UTC');
+    $dtg = date('l jS \of F Y h:i:s A');
 
-	// OK, let's start with the mail redaction.
-	// Who will receive it ?
+    // OK, let's start with the mail redaction.
+    // Who will receive it ?
 
-	$to = "\"Olivier JACQ\" <olivier.jacq@free.fr>";
-	// $to = "\"Olivier JACQ\" <olivier.jacq@free.fr>" . ", ";
-	// $to .= "\"Martin SPOTT\" <martin.spott@mgras.net>";
+    $to = "\"Olivier JACQ\" <olivier.jacq@free.fr>";
+    // $to = "\"Olivier JACQ\" <olivier.jacq@free.fr>" . ", ";
+    // $to .= "\"Martin SPOTT\" <martin.spott@mgras.net>";
 
-	// What is the subject ?
+    // What is the subject ?
 
-	$subject = "[FG Scenery Pending Requests] Automatic shared model pending requests list.";
+    $subject = "[FG Scenery Pending Requests] Automatic shared model pending requests list.";
 
-	// Generating the message and wrapping it to 77 signs per HTML line (asked by Martin). But warning, this must NOT cut an URL, or this will not work.
+    // Generating the message and wrapping it to 77 signs per HTML line (asked by Martin). But warning, this must NOT cut an URL, or this will not work.
 
-	$message0 = "Hi," . "\r\n" .
-	            "This is the automated FG scenery PHP form at:" . "\r\n" .
-		    "http://scenemodels.flightgear.org/submission/pending_submissions.php" . "\r\n" .
-		    "I just wanted to give you a small overview of the requests waiting for validation:" . "\r\n";
-			    
-	$message077 = wordwrap($message0, 77, "\r\n");
+    $message0 = "Hi," . "\r\n" .
+                "This is the automated FG scenery PHP form at:" . "\r\n" .
+                "http://scenemodels.flightgear.org/submission/pending_submissions.php" . "\r\n" .
+                "I just wanted to give you a small overview of the requests waiting for validation:" . "\r\n";
+                        
+    $message077 = wordwrap($message0, 77, "\r\n");
 
-	// There is no possibility to wrap the URL or it will not work, nor the rest of the message (short lines), or it will not work.
+    // There is no possibility to wrap the URL or it will not work, nor the rest of the message (short lines), or it will not work.
 
-	$message1 = $pending_requests."\n";
+    $message1 = $pending_requests."\n";
 
-	$message2 = "They should be somewhere in your mails. Please check again." . "\r\n" .
-				"Thanks!" ;
+    $message2 = "They should be somewhere in your mails. Please check again." . "\r\n" .
+                            "Thanks!" ;
 
-	// Preparing the headers.
+    // Preparing the headers.
 
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "From: \"FG Scenery Pending Requests Form\" <martin.spott@mgras.net>" . "\r\n";
-	$headers .= "X-Mailer: PHP-" . phpversion() . "\r\n";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "From: \"FG Scenery Pending Requests Form\" <martin.spott@mgras.net>" . "\r\n";
+    $headers .= "X-Mailer: PHP-" . phpversion() . "\r\n";
 
-	// Let's send it ! No management of mail() errors to avoid being too talkative...
+    // Let's send it ! No management of mail() errors to avoid being too talkative...
 
-	$message = $message077.$message1.$message2;
+    $message = $message077.$message1.$message2;
 
-	@mail($to, $subject, $message, $headers);
-	}
-	
+    @mail($to, $subject, $message, $headers);
+}
+        
 // Closing the connection.
 
 @pg_close($resource_rw);
