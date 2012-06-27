82b93cf3a456fd1dea5c26b99952cef08359ba13
Julien Nguyen <jnguyen@etu.emse.fr>
2012-06-27 16:49:16 +0200

    Replace iframe from coverage to make it w3c compliant

--- WWW/scenemodels/coverage.php
+++ WWW/scenemodels/coverage.php
@@ -1,7 +1,6 @@
 <?php include 'inc/header.php'; ?>
     <h1>FlightGear Scenery Object Global Coverage</h1>
-    <p>
-    <center>
-    <iframe src="/maps?lat=10&lon=0&zoom=2" width="720px" height="450px"></iframe>
-    </center>
+    <p class="center">
+    <object data="/maps?lat=10&lon=0&zoom=2" type="text/html" height="450px" width=720px"></object>
+    </p>
 <?php include 'inc/footer.php'; ?>
