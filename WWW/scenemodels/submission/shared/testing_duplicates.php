027375bc74c5d4ad35ea56b422453a3ce35065d6
OliverJ <acomv41@yahoo.com>
2012-06-28 16:55:02 +0200

    Duplicated detection support.

--- WWW/scenemodels/submission/shared/testing_duplicates.php
+++ WWW/scenemodels/submission/shared/testing_duplicates.php
@@ -4,7 +4,7 @@ An existing object:
 
 function detect_already_existing_object($lat, $lon, $ob_gndelev, $ob_elevoffset, $ob_heading, $ob_model)<br />
 
-Result : <?php $result = detect_already_existing_object(47.869766, -3.408148, 26.28, 0, 208.00, 520); echo $result; ?><br />
+Result : <?php $result = detect_already_existing_object(47.869766, -3.408148, 26.28, 0, 208, 520); echo $result; ?><br />
 
 A not existing object :</br>
 
