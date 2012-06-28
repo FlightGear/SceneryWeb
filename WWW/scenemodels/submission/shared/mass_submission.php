0515a22285dd5eec3588392046bc5ea994b6ba4c
OliverJ <acomv41@yahoo.com>
2012-06-28 15:19:28 +0200

    Never been so close...

--- WWW/scenemodels/submission/shared/mass_submission.php
+++ WWW/scenemodels/submission/shared/mass_submission.php
@@ -115,7 +115,7 @@ else {
 
     // Managing the cancellation of a mass import by DB maintainer.
 
-    if(((isset($_POST["cancel"])) && (isset($_POST["hsig"])) && (strlen($_POST["hsig"])) == 64) && preg_match("/[0-9a-z]/", $_POST["hsig"]) && ($_POST["submit"] == "Reject - Do not import!")) {
+    if((isset($_POST["cancel"])) && (isset($_POST["hsig"])) && ((strlen($_POST["hsig"])) == 64) && preg_match("/[0-9a-z]/", $_POST["hsig"]) && ($_POST["cancel"] == "Reject - Do not import!")) {
 
          $resource_rw = connect_sphere_rw();
 
@@ -202,6 +202,7 @@ else {
 
     // Now managing the insertion
     if((isset($_POST["submit"])) && (isset($_POST["hsig"])) && ((strlen($_POST["hsig"])) == 64) && preg_match("/[0-9a-z]/", $_POST["hsig"]) && ($_POST["submit"] == "Submit the mass import!")) {
+
         $resource_rw = connect_sphere_rw();
 
         // If connection is OK
