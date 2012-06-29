a5550cf017a9249d6a4e361db50cfb797e981c07
OliverJ <acomv41@yahoo.com>
2012-06-29 17:59:42 +0200

    Testing static submission.

--- WWW/scenemodels/submission/static/static_submission.php
+++ WWW/scenemodels/submission/static/static_submission.php
@@ -6,18 +6,21 @@ $page_title = "Automated Models Submission Form";
 include '../../inc/header.php';
 include '../../inc/geshi/geshi.php';
 
-echo "testing Geshi";
+echo "<p align=\"center\"></p>Hi, this is the static submission. The following model has passed all (numerous) verifications by the forementionned script. It should be fine to validate it. However, it's always sane to eye-check it.</p>";
+
 // Geshi stuff
     $source = file_get_contents('test.xml');
     $language = 'xml';
     $geshi = new GeSHi($source, $language);
+    $geshi->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS);
+    $geshi->set_line_style('background: #fcfcfc;');
 
     geshi_highlight($source, 'xml', $path);
 ?>
 
 <a href="ContainerCrane.png" rel="lightbox[submission]" title="1st texture">image #1</a>
-<a href="Gasometer.png" rel="lightbox[submission]" title="2nd texture">image #2</a>
-<a href="ladder.png" rel="lightbox[submission]" title="3rd texture">image #3</a>
+<a href="ATR42BR0.bmp" rel="lightbox[submission]" title="2nd texture">image #2</a>
+<a href="ATR42BR2.bmp" rel="lightbox[submission]" title="3rd texture">image #3</a>
 
 <?php
 include '../../inc/footer.php';
