e1d11989a200495f810bd37f5276c9b6ae2986b2
OliverJ <acomv41@yahoo.com>
2012-07-04 15:59:25 +0200

    Trying something.

--- WWW/scenemodels/index_test.php
+++ WWW/scenemodels/index_test.php
@@ -31,7 +31,7 @@ $affimage = rand(1, 2477);
             <?php
             for ($i=0; $i<2477; $i++) {
                     if ($i == $affimage) {
-                        echo "<a href=\"http://scenemodels.flightgear.org/modeledit.php?id=".$i." rel=\"lightbox[submission]\" title=\"1st texture\"><img src=\"modelthumb.php?id=".$affimage."\"></a>\n";
+                        echo "<a href=\"http://scenemodels.flightgear.org/modeledit.php?id=".$i."\" rel=\"lightbox[submission]\" title=\"".$i."st texture\"><img src=\"modelthumb.php?id=".$affimage."\"></a>\n";
                     }
                     else
                         echo "<a href=\"http://scenemodels.flightgear.org/modeledit.php?id=".$i." rel=\"lightbox[submission]\" title=\"1st texture\"></a>\n";
