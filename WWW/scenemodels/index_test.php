227fbea0de4b632597644909d0f64df5da89dcd1
OliverJ <acomv41@yahoo.com>
2012-07-04 15:13:27 +0200

    Trying something.

--- WWW/scenemodels/index_test.php
+++ WWW/scenemodels/index_test.php
@@ -15,12 +15,12 @@ $affimage = rand(1, 2477);
 // There is still the problem of the "no image". Should have to do a md5sum on the Base64 to check the returned image is not blank.
 ?>
 
-<table style="border-style: solid; border-width: 1px;\" cellpadding="1\" cellspacing="1" rules="rows">
+<table style="border-style: solid; border-width: 1px;" cellpadding="1" cellspacing="1" rules="rows">
         <tr>
         <td>
             <iframe
                 src="http://mapserver.flightgear.org/lightmap/?lon=-117.12099&amp;lat=32.73356&amp;zoom=12"
-                width="320" height="240"
+                width="480" height="360"
                 scrolling="no"
                 marginwidth="2" marginheight="2"
                 frameborder="0">
@@ -29,7 +29,7 @@ $affimage = rand(1, 2477);
         <td>
             <iframe
                 src="modelthumb.php?id=<?php echo $affimage; ?>"
-                width="320" height="240"
+                width="480" height="360"
                 scrolling="no"
                 marginwidth="2" marginheight="2"
                 frameborder="0" alt="Random picture">
