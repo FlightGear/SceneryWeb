b34145f63d307177928dbb2affe82ed2d3914881
OliverJ <acomv41@yahoo.com>
2012-07-04 22:04:55 +0200

    Website organisation.

--- WWW/scenemodels/scenery_download.php
+++ WWW/scenemodels/scenery_download.php
@@ -2,10 +2,9 @@
 $page_title = "FlightGear World Scenery v2.6.0";
 include 'inc/header.php';
 ?>
-<p class="center">
+<br />
 <iframe src="http://www.flightgear.org/legacy-Downloads/scenery-2.6.0.html" height="600" width="800"
 frameborder="0" scrolling="no"></iframe>
-</p>
 <?php
 include('inc/footer.php');
 ?>
