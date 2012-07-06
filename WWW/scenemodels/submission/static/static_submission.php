ee2f1147cf7075c428b44fdcb0dba8a0b3687118
OliverJ <acomv41@yahoo.com>
2012-07-06 10:43:08 +0200

    Trying to get model files from DB.

--- WWW/scenemodels/submission/static/static_submission.php
+++ WWW/scenemodels/submission/static/static_submission.php
@@ -454,12 +454,12 @@ else {
 <?php
 // Now (hopefully) trying to manage the AC3D + XML + PNG texture files stuff
 
-    unlink('/tmp/submission/submitted_files.tar.gz');  // Deletes compressed file
-    clearDir('/tmp/submission');                       // Deletes temporary submission directory
+    //unlink('/tmp/submission/submitted_files.tar.gz');  // Deletes compressed file
+    //clearDir('/tmp/submission');                       // Deletes temporary submission directory
 
-    while (file_exists('/tmp/submission')) {
-        usleep(500);    // Makes concurrent access impossible: the script has to wait if this directory already exists.
-    }
+    //while (file_exists('/tmp/submission')) {
+    //    usleep(500);    // Makes concurrent access impossible: the script has to wait if this directory already exists.
+    //}
 
     if (!mkdir('/tmp/submission/')) {
         echo "Impossible to create '/tmp/submission/' directory!";
