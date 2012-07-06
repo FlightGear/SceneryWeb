79e74ca8e0ed4be5a303288e915fc9dae6649404
OliverJ <acomv41@yahoo.com>
2012-07-06 15:03:18 +0200

    Trying to get model files from DB.

--- WWW/scenemodels/submission/static/static_submission.php
+++ WWW/scenemodels/submission/static/static_submission.php
@@ -426,7 +426,7 @@ else {
         <td>Map</td>
         <td>
         <center>
-        <iframe src="http://mapserver.flightgear.org/map/?lon=<?php echo $ob_long; ?>&lat=<?php echo $ob_lat; ?>&zoom=14&layers=000B0000TFFFTFFFTFTFTFFF13" width="320" height="240" scrolling="auto" marginwidth="2" marginheight="2" frameborder="0">
+        <iframe src="http://mapserver.flightgear.org/map/?lon=<?php echo $ob_long; ?>&lat=<?php echo $ob_lat; ?>&zoom=14&layers=000B0000TFFFTFFFTFTFFFFF" width="320" height="240" scrolling="auto" marginwidth="2" marginheight="2" frameborder="0">
         </iframe>
         </center>
         </td>
@@ -477,7 +477,8 @@ else {
     $toto = new PharData($file);
     foreach($toto as $fichier) {
         echo $fichier."<br />";
-}
+        $toto->decompress();
+    }
 
     //$phar->decompressFiles();
 
