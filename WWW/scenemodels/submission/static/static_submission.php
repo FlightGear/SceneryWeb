5832c76db9da16c86ce5006de71572359db6bf02
OliverJ <acomv41@yahoo.com>
2012-07-06 11:10:52 +0200

    Trying to get model files from DB.

--- WWW/scenemodels/submission/static/static_submission.php
+++ WWW/scenemodels/submission/static/static_submission.php
@@ -472,6 +472,7 @@ else {
         file_put_contents ($file, $archive);            // Writes the content of $mo_modelfile into submitted_file.tar.gz
 
         $last_line = system('gunzip /tmp/submission/submitted_files.tar.gz');
+        $last_line = system('tar x /tmp/submission/submitted_files.tar');
 
         $dir = opendir("/tmp/submission");
         while($file = readdir($dir)) {
