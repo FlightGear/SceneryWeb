77fa2bc370d073187397cc053834bf61ddaef10e
Martin Spott <Martin.Spott@mgras.net>
2012-07-07 09:03:51 +0200

    Further consolidation of OL maps

--- WWW/mapserver/submap/index.php
+++ WWW/mapserver/submap/index.php
@@ -6,9 +6,19 @@
             .olPopup p { margin:0px; font-size: .9em;}
             h2 { margin:0px; font-size: 1.2em;}
         </style>
-        <!-- <script type="text/javascript" src="http://www.openlayers.org/api/OpenLayers.js"></script> -->
-        <script type="text/javascript" src="http://mapserver.flightgear.org/ol/OpenLayers.js"></script>
-        <script src="http://api.maps.yahoo.com/ajaxymap?v=3.0&appid=euzuro-openlayers"></script>
+
+<!--    <link rel="stylesheet" href="http://www.openlayers.org/dev/theme/default/style.css" type="text/css" />
+    <link rel="stylesheet" href="http://www.openlayers.org/dev/examples/style.css" type="text/css" /> -->
+<!--    <script src="http://www.openlayers.org/dev/OpenLayers.js"></script> -->
+    <script src="/ol/OpenLayers.js"></script>
+<!--    <script src="../openlayers-ys/OpenLayers.js"></script> -->
+
+    <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAVLFq91rDGGNi1LlKdN1PxBR0Q4haDqJCswRe1MDQbYGWGgDI3xTCcUDGymGT0ezb2XnDp9Yx3wF9Kw"></script>
+    <script src="http://api.maps.yahoo.com/ajaxymap?v=3.0&appid=euzuro-openlayers"></script>
+<!--    <script src='http://dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=6.1'></script> -->
+    <script src="http://www.openstreetmap.org/openlayers/OpenStreetMap.js"></script>
+    <script src='http://mapserver.flightgear.org/map/maplayers.js' type='text/javascript'></script>
+    <script src='http://mapserver.flightgear.org/map/downloadbox.js' type='text/javascript'></script>
 
         <script type="text/javascript">
         <!--
@@ -16,30 +26,6 @@
         var lat = <?php print $_REQUEST["lat"]; ?>;
         var zoom = <?php print $_REQUEST["zoom"]; ?>;
         var map;
-
-        function get_osm_url (bounds) {
-            var res = this.map.getResolution();
-            var x = Math.round ((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
-            var y = Math.round ((this.maxExtent.top - bounds.top) / (res * this.tileSize.h));
-            var z = this.map.getZoom();
-            var limit = Math.pow(2, z);
-            if (y < 0 || y >= limit)
-            {
-                return null;
-            }
-            else
-            {
-                x = ((x % limit) + limit) % limit;
-
-                var path = z + "/" + x + "/" + y + "." + this.type;
-                var url = this.url;
-                if (url instanceof Array) {
-                    url = this.selectUrl(path, url);
-                }
-                return url + path;
-            }
-        }
-
 
         function init() {
 
@@ -54,63 +40,6 @@
                 maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
                 numZoomLevels:18, maxResolution:156543.0339, units:'m', projection: "EPSG:900913"}
             );
-
-            var yahoosat = new OpenLayers.Layer.Yahoo( "Yahoo Satellite (License!?)",
-                {type: YAHOO_MAP_SAT, 'sphericalMercator': true, numZoomLevels: 20}
-            );
-
-            var icubed = new OpenLayers.Layer.WMS( "Telascience i-Cubed",
-                [ "http://t1.hypercube.telascience.org/tiles?",
-                "http://t2.hypercube.telascience.org/tiles?",
-                "http://t3.hypercube.telascience.org/tiles?",
-                "http://t4.hypercube.telascience.org/tiles?" ],
-                {layers: 'landsat7-google', format: 'image/jpeg'} ,
-                {wrapDateLine: true} );
-
-            var tarmac = new OpenLayers.Layer.WMS( "Airfield layouts",
-                [ "http://1.flightgear.telascience.org/tc?",
-                "http://2.flightgear.telascience.org/tc?",
-                "http://3.flightgear.telascience.org/tc?",
-                "http://4.flightgear.telascience.org/tc?" ],
-                {layers: 'tarmac', transparent: 'true', format: 'image/png'},
-                {isBaseLayer: false} );
-
-            var wmssigns = new OpenLayers.Layer.WMS( "WMS Taxiway Signs",
-                [ "http://2.flightgear.telascience.org/ms?srs=EPSG%3A900913&",
-                "http://4.flightgear.telascience.org/ms?srs=EPSG%3A900913&" ],
-                {layers: 'fgs_signs', transparent: 'true', format: 'image/png'},
-                {isBaseLayer: false, maxScale: 5000 });
-
-            var wfssigns = new OpenLayers.Layer.WFS( "WFS Taxiway Signs",
-                "http://scenemodels.flightgear.org/ms?srs=EPSG%3A900913&",
-                { typename: "fgs_signs", maxfeatures: 200},
-                { minScale: 5000 });
-
-            var osmlines = new OpenLayers.Layer.WMS( "Line features",
-                [ "http://1.flightgear.telascience.org/tc?",
-                "http://2.flightgear.telascience.org/tc?",
-                "http://3.flightgear.telascience.org/tc?",
-                "http://4.flightgear.telascience.org/tc?" ],
-                {layers: 'osmlines', transparent: 'true', format: 'image/png'},
-                {isBaseLayer: false} );
-
-            var mapnik = new OpenLayers.Layer.TMS(
-                "mapnik",
-                "http://tile.openstreetmap.org/mapnik/",
-                {type:'png', getURL: get_osm_url, displayOutsideMaxExtent: true, 'buffer':1, transitionEffect: 'resize'} );
-
-            var osmarender = new OpenLayers.Layer.TMS(
-                "osmarender",
-                ["http://a.tah.openstreetmap.org/Tiles/tile/", "http://b.tah.openstreetmap.org/Tiles/tile/", "http://c.tah.openstreetmap.org/Tiles/tile/"],
-                {type:'png', getURL: get_osm_url, displayOutsideMaxExtent: true, 'buffer':1, transitionEffect: 'resize'} );
-
-            var wmsobjects = new OpenLayers.Layer.WMS( "Scenery Objects",
-                [ "http://2.flightgear.telascience.org/ms?srs=EPSG%3A900913&",
-                "http://4.flightgear.telascience.org/ms?srs=EPSG%3A900913&" ],
-                {layers: 'fgs_staticobjects,fgs_sharedobjects', transparent: 'true', format: 'image/png'},
-                {isBaseLayer: false, maxScale: 12500 });
-
-
 
             map.addLayers([yahoosat, mapnik, osmarender, tarmac, osmlines, wmsobjects, wmssigns, wfssigns]);
             map.addControl(new OpenLayers.Control.KeyboardDefaults());
