e6fe4ccded968c55a671849a649a0126e96b1762
Martin Spott <Martin.Spott@mgras.net>
2012-07-07 09:41:00 +0200

    Further cleanup on OL maps

--- WWW/mapserver/popmap/index.php
+++ WWW/mapserver/popmap/index.php
@@ -7,18 +7,12 @@
             h2 { margin:0px; font-size: 1.2em;}
         </style>
 
-<!--    <link rel="stylesheet" href="http://www.openlayers.org/dev/theme/default/style.css" type="text/css" />
-    <link rel="stylesheet" href="http://www.openlayers.org/dev/examples/style.css" type="text/css" /> -->
-<!--    <script src="http://www.openlayers.org/dev/OpenLayers.js"></script> -->
-    <script src="/ol/OpenLayers.js"></script>
-<!--    <script src="../openlayers-ys/OpenLayers.js"></script> -->
+        <script src="/ol/OpenLayers.js"></script>
 
-    <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAVLFq91rDGGNi1LlKdN1PxBR0Q4haDqJCswRe1MDQbYGWGgDI3xTCcUDGymGT0ezb2XnDp9Yx3wF9Kw"></script>
-    <script src="http://api.maps.yahoo.com/ajaxymap?v=3.0&appid=euzuro-openlayers"></script>
-<!--    <script src='http://dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=6.1'></script> -->
-    <script src="http://www.openstreetmap.org/openlayers/OpenStreetMap.js"></script>
-    <script src='http://mapserver.flightgear.org/map/maplayers.js' type='text/javascript'></script>
-    <script src='http://mapserver.flightgear.org/map/downloadbox.js' type='text/javascript'></script>
+        <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAVLFq91rDGGNi1LlKdN1PxBR0Q4haDqJCswRe1MDQbYGWGgDI3xTCcUDGymGT0ezb2XnDp9Yx3wF9Kw"></script>
+        <script src="http://api.maps.yahoo.com/ajaxymap?v=3.0&appid=euzuro-openlayers"></script>
+        <script src="http://www.openstreetmap.org/openlayers/OpenStreetMap.js"></script>
+        <script src='http://mapserver.flightgear.org/map/maplayers.js' type='text/javascript'></script>
 
         <script type="text/javascript">
         <!--
