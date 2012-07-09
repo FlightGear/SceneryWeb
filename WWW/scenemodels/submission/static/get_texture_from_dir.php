a18950d7f366a0084e953097aa4e8bc844e72df1
OliverJ <acomv41@yahoo.com>
2012-07-09 10:57:15 +0200

    Files from DB went OK, now testing.

--- WWW/scenemodels/submission/static/get_texture_from_dir.php
+++ WWW/scenemodels/submission/static/get_texture_from_dir.php
@@ -1,6 +1,9 @@
 <?php
 
-// The goal of this small file is to display the thumnail of a pending model request in the fgs_position_requests table.
+// Inserting libs
+require_once ('../../inc/functions.inc.php');
+
+// The goal of this small file is to display the texture of a pending model request in the fgs_position_requests table.
 // There is no other (known ;-) possibility to include this in the rest of the static submission script.
 
 header("Content-type: image/jpg");
