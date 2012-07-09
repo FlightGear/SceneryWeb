838b9ff34bc4d2ce720e53593569152478fb313d
OliverJ <acomv41@yahoo.com>
2012-07-09 16:28:46 +0200

    Centering stuff.

--- WWW/scenemodels/submission/shared/check_mass_import.php
+++ WWW/scenemodels/submission/shared/check_mass_import.php
@@ -49,10 +49,10 @@ else {
     //(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
     if((isset($_POST['email'])) && ((strlen($_POST['email']))>0) && ((strlen($_POST['email'])<=50))) {
         $safe_email = pg_escape_string(stripslashes($_POST['email']));
-        echo "<font color=\"green\">Email: ".$safe_email."</font><br />";
+        echo "<center><font color=\"green\">Email: ".$safe_email."</font></center><br />";
     }
     else {
-        echo "<font color=\"red\">No email was given (not mandatory) or email mismatch!</font><br />";
+        echo "<center><font color=\"red\">No email was given (not mandatory) or email mismatch!</font></center><br />";
         $failed_mail = 1;
     }
 
