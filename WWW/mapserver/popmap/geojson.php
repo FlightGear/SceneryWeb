ec7d4d75add8756062a50bc80bed1f3e6835443b
OliverJ <acomv41@yahoo.com>
2012-07-10 11:49:18 +0200

    Trying to add edition and deletion links.

--- WWW/mapserver/popmap/geojson.php
+++ WWW/mapserver/popmap/geojson.php
@@ -1,351 +1,45 @@
+<?php
+// Connecting to the database
+$link = pg_connect('dbname='.$dbname.' host='.$dbhost.' user='.$dbuser.' password='.$dbpass.' sslmode=disable');
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
-          "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-  
-<meta http-equiv="X-UA-Compatible" content="chrome=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>mapserver/popmap/geojson.php - sceneryweb in FlightGear - Gitorious</title>
-<link href="/stylesheets/gts-common.css?1339069040" media="screen" rel="stylesheet" type="text/css" />
-<link href="//fonts.googleapis.com/css?family=Nobile&amp;v1" type="text/css" rel="stylesheet">
+// Is any boundary box defined?
+    if (isset($_REQUEST['bbox']) && (preg_match('/^[0-9\,\-\.]+$/u',$_GET['bbox']))) {
+        $bounds = explode(",",$_REQUEST['bbox']);
+        //echo $bounds[0]." ".$bounds[1]." ".$bounds[2]." ".$bounds[3]."\n";
+    }
 
-<script src="/javascripts/all.js?1339069067" type="text/javascript"></script>      <link href="/stylesheets/prettify/prettify.css?1339069040" media="screen" rel="stylesheet" type="text/css" />    <script src="/javascripts/lib/prettify.js?1339069040" type="text/javascript"></script>      <!--[if IE 8]><link rel="stylesheet" href="/stylesheets/ie8.css" type="text/css"><![endif]-->
-<!--[if IE 7]><link rel="stylesheet" href="/stylesheets/ie7.css" type="text/css"><![endif]-->
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-52238-3']);
-_gaq.push(['_setDomainName', '.gitorious.org'])
-_gaq.push(['_trackPageview']);
-(function() {
-   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
-})();
-</script>
-</head>
-<body id="blobs">
-  <div id="wrapper">
-        <ul id="user-nav">
-      <li><a href="/">Dashboard</a></li>
-      
-    <li class="secondary"><a href="/~dumbojet">~dumbojet</a></li>
-  <li class="secondary messages ">
-          <a href="/messages"><span>0</span></a>      </li>
-  <li class="secondary subtle"><a href="/logout">Logout</a></li>
-    </ul>
-    <div id="header">
-      <h1 id="logo">
-        <a href="/"><img alt="Logo" src="/img/logo.png?1294322727" /></a>
-      </h1>
-      <ul id="menu">
-                  <li class="activity"><a href="/activities">Activities</a></li>
-          <li class="projects"><a href="/projects">Projects</a></li>
-          <li class="teams"><a href="/teams">Teams</a></li>
-              </ul>
-    </div>
-    <div id="top-bar">
-      <ul id="breadcrumbs">
-        <li class="project"><a href="/fg">FlightGear</a></li><li class="repository"><a href="/fg/sceneryweb">sceneryweb</a></li><li class="branch"><a href="/fg/sceneryweb/commits/master">master</a></li><li class="tree"><a href="/fg/sceneryweb/trees/master">/</a></li><li class="folder"><a href="/fg/sceneryweb/trees/master/mapserver">mapserver</a></li><li class="folder"><a href="/fg/sceneryweb/trees/master/mapserver/popmap">popmap</a></li><li class="file"><a href="/fg/sceneryweb/blobs/master/mapserver/popmap/geojson.php">geojson.php</a></li>      </ul>
-              <div id="searchbox">
-          
+    else {
+        echo "No bbox defined!\n";
+    }
 
+// Preparing the query
+$query = "SELECT ob_id, ob_text, ob_model, ST_Y(wkb_geometry) AS ob_lat, ST_X(wkb_geometry) AS ob_lon, ob_heading ";
+$query.= "FROM fgs_objects ";
+$query.= "WHERE ST_Within(wkb_geometry, ST_GeomFromText('POLYGON((".$bounds[0]." ".$bounds[1].",".$bounds[0]." ".$bounds[3].",".$bounds[2]." ".$bounds[3].",".$bounds[2]." ".$bounds[1].",".$bounds[0]." ".$bounds[1]."))',4326)) ";
+$query.= "LIMIT 400";
+//echo $query."\n\n\n";
 
-<div class="search_bar">
-  <form action="/search" method="get">    <p>
-      <input class="text search-field round-5" id="q" name="q" type="text" />      <input type="submit" value="Search" class="search-submit round-5" />
-    </p>
-    <p class="hint search-hint" style="display: none;">
-      eg. 'wrapper', 'category:python' or '"document database"'
-          </p>
-  </form></div>
-        </div>
-          </div>
-    <div id="container" class="">
-      <div id="content" class="">
-        
-        
-
-
-
-<div class="page-meta">
-  <ul class="page-actions">
-    <li>Blob contents</li>
-    <li><a href="/fg/sceneryweb/blobs/blame/6d64f4166882d17aeba0ad1c36aeb71ee80ebe14/mapserver/popmap/geojson.php" class="blame js-pjax" data-pjax="#codeblob">Blame</a></li>
-    <li><a href="/fg/sceneryweb/blobs/history/master/mapserver/popmap/geojson.php" class="js-pjax" data-pjax="#codeblob">Blob history</a></li>
-    <li><a href="/fg/sceneryweb/blobs/raw/master/mapserver/popmap/geojson.php">Raw blob data</a></li>
-  </ul>
-</div>
-
-
-
-<!-- mime: application/httpd-php -->
-
-       <div id="long-file" style="display:none"
-                  class="help-box center error round-5">
-               <div class="icon error"></div>        <p>
-          This file looks large and may slow your browser down if we attempt
-          to syntax highlight it, so we are showing it without any
-          pretty colors.
-          <a href="#highlight-anyway" id="highlight-anyway">Highlight
-          it anyway</a>.
-        </p>
-     </div>    <table id="codeblob" class="highlighted lang-php">
-<tr id="line1">
-<td class="line-numbers"><a href="#line1" name="line1">1</a></td>
-<td class="code"><pre class="prettyprint lang-php">&lt;?php</pre></td>
-</tr>
-<tr id="line2">
-<td class="line-numbers"><a href="#line2" name="line2">2</a></td>
-<td class="code"><pre class="prettyprint lang-php"></pre></td>
-</tr>
-<tr id="line3">
-<td class="line-numbers"><a href="#line3" name="line3">3</a></td>
-<td class="code"><pre class="prettyprint lang-php">// Connecting to the database</pre></td>
-</tr>
-<tr id="line4">
-<td class="line-numbers"><a href="#line4" name="line4">4</a></td>
-<td class="code"><pre class="prettyprint lang-php">$link = pg_connect('dbname='.$dbname.' host='.$dbhost.' user='.$dbuser.' password='.$dbpass.' sslmode=disable');</pre></td>
-</tr>
-<tr id="line5">
-<td class="line-numbers"><a href="#line5" name="line5">5</a></td>
-<td class="code"><pre class="prettyprint lang-php"></pre></td>
-</tr>
-<tr id="line6">
-<td class="line-numbers"><a href="#line6" name="line6">6</a></td>
-<td class="code"><pre class="prettyprint lang-php">// Is any boundary box defined?</pre></td>
-</tr>
-<tr id="line7">
-<td class="line-numbers"><a href="#line7" name="line7">7</a></td>
-<td class="code"><pre class="prettyprint lang-php"></pre></td>
-</tr>
-<tr id="line8">
-<td class="line-numbers"><a href="#line8" name="line8">8</a></td>
-<td class="code"><pre class="prettyprint lang-php">    if (isset($_REQUEST['bbox']) &amp;&amp; (preg_match('/^[0-9\,\-\.]+$/u',$_GET['bbox']))) {</pre></td>
-</tr>
-<tr id="line9">
-<td class="line-numbers"><a href="#line9" name="line9">9</a></td>
-<td class="code"><pre class="prettyprint lang-php">        $bounds = explode(&quot;,&quot;,$_REQUEST['bbox']);</pre></td>
-</tr>
-<tr id="line10">
-<td class="line-numbers"><a href="#line10" name="line10">10</a></td>
-<td class="code"><pre class="prettyprint lang-php">        //echo $bounds[0].&quot; &quot;.$bounds[1].&quot; &quot;.$bounds[2].&quot; &quot;.$bounds[3].&quot;\n&quot;;</pre></td>
-</tr>
-<tr id="line11">
-<td class="line-numbers"><a href="#line11" name="line11">11</a></td>
-<td class="code"><pre class="prettyprint lang-php">    }</pre></td>
-</tr>
-<tr id="line12">
-<td class="line-numbers"><a href="#line12" name="line12">12</a></td>
-<td class="code"><pre class="prettyprint lang-php">    else {</pre></td>
-</tr>
-<tr id="line13">
-<td class="line-numbers"><a href="#line13" name="line13">13</a></td>
-<td class="code"><pre class="prettyprint lang-php">        echo &quot;No bbox defined!\n&quot;;</pre></td>
-</tr>
-<tr id="line14">
-<td class="line-numbers"><a href="#line14" name="line14">14</a></td>
-<td class="code"><pre class="prettyprint lang-php">    }</pre></td>
-</tr>
-<tr id="line15">
-<td class="line-numbers"><a href="#line15" name="line15">15</a></td>
-<td class="code"><pre class="prettyprint lang-php"></pre></td>
-</tr>
-<tr id="line16">
-<td class="line-numbers"><a href="#line16" name="line16">16</a></td>
-<td class="code"><pre class="prettyprint lang-php">// Preparing the query</pre></td>
-</tr>
-<tr id="line17">
-<td class="line-numbers"><a href="#line17" name="line17">17</a></td>
-<td class="code"><pre class="prettyprint lang-php">$query = &quot;SELECT ob_id, ob_text, ob_model, ST_Y(wkb_geometry) AS ob_lat, ST_X(wkb_geometry) AS ob_lon, ob_heading &quot;;</pre></td>
-</tr>
-<tr id="line18">
-<td class="line-numbers"><a href="#line18" name="line18">18</a></td>
-<td class="code"><pre class="prettyprint lang-php">$query.= &quot;FROM fgs_objects &quot;;</pre></td>
-</tr>
-<tr id="line19">
-<td class="line-numbers"><a href="#line19" name="line19">19</a></td>
-<td class="code"><pre class="prettyprint lang-php">$query.= &quot;WHERE ST_Within(wkb_geometry, ST_GeomFromText('POLYGON((&quot;.$bounds[0].&quot; &quot;.$bounds[1].&quot;,&quot;.$bounds[0].&quot; &quot;.$bounds[3].&quot;,&quot;.$bounds[2].&quot; &quot;.$bounds[3].&quot;,&quot;.$bounds[2].&quot; &quot;.$bounds[1].&quot;,&quot;.$bounds[0].&quot; &quot;.$bounds[1].&quot;))',4326)) &quot;;</pre></td>
-</tr>
-<tr id="line20">
-<td class="line-numbers"><a href="#line20" name="line20">20</a></td>
-<td class="code"><pre class="prettyprint lang-php">$query.= &quot;LIMIT 400&quot;;</pre></td>
-</tr>
-<tr id="line21">
-<td class="line-numbers"><a href="#line21" name="line21">21</a></td>
-<td class="code"><pre class="prettyprint lang-php"></pre></td>
-</tr>
-<tr id="line22">
-<td class="line-numbers"><a href="#line22" name="line22">22</a></td>
-<td class="code"><pre class="prettyprint lang-php">    //echo $query.&quot;\n\n\n&quot;;</pre></td>
-</tr>
-<tr id="line23">
-<td class="line-numbers"><a href="#line23" name="line23">23</a></td>
-<td class="code"><pre class="prettyprint lang-php">?&gt;</pre></td>
-</tr>
-<tr id="line24">
-<td class="line-numbers"><a href="#line24" name="line24">24</a></td>
-<td class="code"><pre class="prettyprint lang-php">{&quot;type&quot;:&quot;FeatureCollection&quot;,</pre></td>
-</tr>
-<tr id="line25">
-<td class="line-numbers"><a href="#line25" name="line25">25</a></td>
-<td class="code"><pre class="prettyprint lang-php">    &quot;features&quot;:[</pre></td>
-</tr>
-<tr id="line26">
-<td class="line-numbers"><a href="#line26" name="line26">26</a></td>
-<td class="code"><pre class="prettyprint lang-php">        &lt;?php</pre></td>
-</tr>
-<tr id="line27">
-<td class="line-numbers"><a href="#line27" name="line27">27</a></td>
-<td class="code"><pre class="prettyprint lang-php">            // Grabbing data from query</pre></td>
-</tr>
-<tr id="line28">
-<td class="line-numbers"><a href="#line28" name="line28">28</a></td>
-<td class="code"><pre class="prettyprint lang-php">            $result = pg_query($query);</pre></td>
-</tr>
-<tr id="line29">
-<td class="line-numbers"><a href="#line29" name="line29">29</a></td>
-<td class="code"><pre class="prettyprint lang-php">            while ($row = pg_fetch_assoc($result)){</pre></td>
-</tr>
-<tr id="line30">
-<td class="line-numbers"><a href="#line30" name="line30">30</a></td>
-<td class="code"><pre class="prettyprint lang-php">        ?&gt;</pre></td>
-</tr>
-<tr id="line31">
-<td class="line-numbers"><a href="#line31" name="line31">31</a></td>
-<td class="code"><pre class="prettyprint lang-php">        {&quot;type&quot;: &quot;Feature&quot;,</pre></td>
-</tr>
-<tr id="line32">
-<td class="line-numbers"><a href="#line32" name="line32">32</a></td>
-<td class="code"><pre class="prettyprint lang-php">            &quot;id&quot;: &quot;OpenLayers.Feature.Vector_&lt;?php echo $row[&quot;ob_id&quot;];?&gt;&quot;,</pre></td>
-</tr>
-<tr id="line33">
-<td class="line-numbers"><a href="#line33" name="line33">33</a></td>
-<td class="code"><pre class="prettyprint lang-php">            &quot;properties&quot;:{</pre></td>
-</tr>
-<tr id="line34">
-<td class="line-numbers"><a href="#line34" name="line34">34</a></td>
-<td class="code"><pre class="prettyprint lang-php">                &quot;heading&quot;: &lt;?php echo $row[&quot;ob_heading&quot;];?&gt;,</pre></td>
-</tr>
-<tr id="line35">
-<td class="line-numbers"><a href="#line35" name="line35">35</a></td>
-<td class="code"><pre class="prettyprint lang-php">                &quot;title&quot;: &quot;Object #&lt;?php echo $row[&quot;ob_id&quot;];?&gt; - &lt;?php echo $row[&quot;ob_text&quot;];?&gt;&quot;,</pre></td>
-</tr>
-<tr id="line36">
-<td class="line-numbers"><a href="#line36" name="line36">36</a></td>
-<td class="code"><pre class="prettyprint lang-php">                &quot;description&quot;: &quot;&lt;img src=http://scenemodels.flightgear.org/modelthumb.php?id=&lt;?php echo $row[&quot;ob_model&quot;];?&gt;&gt;&quot;</pre></td>
-</tr>
-<tr id="line37">
-<td class="line-numbers"><a href="#line37" name="line37">37</a></td>
-<td class="code"><pre class="prettyprint lang-php">            },</pre></td>
-</tr>
-<tr id="line38">
-<td class="line-numbers"><a href="#line38" name="line38">38</a></td>
-<td class="code"><pre class="prettyprint lang-php">            &quot;geometry&quot;:{</pre></td>
-</tr>
-<tr id="line39">
-<td class="line-numbers"><a href="#line39" name="line39">39</a></td>
-<td class="code"><pre class="prettyprint lang-php">                &quot;type&quot;: &quot;Point&quot;,&quot;coordinates&quot;: [&lt;?php echo $row[&quot;ob_lon&quot;];?&gt;, &lt;?php echo $row[&quot;ob_lat&quot;];?&gt;]</pre></td>
-</tr>
-<tr id="line40">
-<td class="line-numbers"><a href="#line40" name="line40">40</a></td>
-<td class="code"><pre class="prettyprint lang-php">            },</pre></td>
-</tr>
-<tr id="line41">
-<td class="line-numbers"><a href="#line41" name="line41">41</a></td>
-<td class="code"><pre class="prettyprint lang-php">        },</pre></td>
-</tr>
-<tr id="line42">
-<td class="line-numbers"><a href="#line42" name="line42">42</a></td>
-<td class="code"><pre class="prettyprint lang-php">        &lt;?php</pre></td>
-</tr>
-<tr id="line43">
-<td class="line-numbers"><a href="#line43" name="line43">43</a></td>
-<td class="code"><pre class="prettyprint lang-php">            }</pre></td>
-</tr>
-<tr id="line44">
-<td class="line-numbers"><a href="#line44" name="line44">44</a></td>
-<td class="code"><pre class="prettyprint lang-php">        ?&gt;</pre></td>
-</tr>
-<tr id="line45">
-<td class="line-numbers"><a href="#line45" name="line45">45</a></td>
-<td class="code"><pre class="prettyprint lang-php">    ]</pre></td>
-</tr>
-<tr id="line46">
-<td class="line-numbers"><a href="#line46" name="line46">46</a></td>
-<td class="code"><pre class="prettyprint lang-php">}</pre></td>
-</tr>
-</table>  
-<script type="text/javascript" charset="utf-8">
-  (function () {
-      if ($("#codeblob tr td.line-numbers:last").text().length < 3500) {
-          prettyPrint();
-      } else {
-          $("#long-file").show().find("a#highlight-anyway").click(function(e){
-              prettyPrint();
-              e.preventDefault();
-          });
-      }
-  }());
-</script>
-
-      </div>
-          </div>
-    <div id="footer">
-      
-<div class="powered-by">
-  <a href="http://gitorious.org"><img alt="Poweredby" src="/images/../img/poweredby.png?1294322727" title="Powered by Gitorious" /></a></div>
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-52238-3']);
-_gaq.push(['_setDomainName', '.gitorious.org'])
-_gaq.push(['_trackPageview']);
-(function() {
-   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
-})();
-</script><script src="/javascripts/onload.js?1339069040" type="text/javascript"></script>
-      
-<div id="footer-links">
-  <h3>Gitorious</h3>
-  <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/about">About</a></li>
-    <li><a href="/about/faq">FAQ</a></li>
-    <li><a href="/contact">Contact</a></li>
-  </ul>
-  
-    <ul>
-      <li><a href="http://groups.google.com/group/gitorious">Discussion group</a></li>
-      <li><a href="http://blog.gitorious.org">Blog</a></li>
-    </ul>
-  
-      
-<ul>
-  <li><a href="http://en.gitorious.org/tos">Terms of Service</a></li>
-  <li><a href="http://en.gitorious.org/privacy_policy">Privacy Policy</a></li>
-</ul>
-
-  
-  
-    <ul>
-      
-        <li><a href="http://gitorious.com/">Professional Gitorious Services</a></li>
-      
-    </ul>
-  
-</div>
-      <p class="footer-blurb">
-  
-    <a href="http://gitorious.com">Professional Gitorious services</a> - Git
-    hosting at your company, custom features, support and more.
-    <a href="http://gitorious.com">gitorious.com</a>.
-  
-</p>
-
-      <div class="clear"></div>
-    </div>
-  </div>
-</body>
-</html>
+?>
+{"type":"FeatureCollection",
+    "features":[
+        <?php
+            // Grabbing data from query
+            $result = pg_query($query);
+            while ($row = pg_fetch_assoc($result)){
+        ?>
+        {"type": "Feature",
+            "id": "OpenLayers.Feature.Vector_<?php echo $row["ob_id"];?>",
+            "properties":{
+                "heading": <?php echo $row["ob_heading"];?>,
+                "title": "Object #<?php echo $row["ob_id"];?> - <?php echo $row["ob_text"];?>",
+                "description": "<img src=http://scenemodels.flightgear.org/modelthumb.php?id=<?php echo $row["ob_model"];?>>"
+            },
+            "geometry":{
+                "type": "Point","coordinates": [<?php echo $row["ob_lon"];?>, <?php echo $row["ob_lat"];?>]
+            },
+        },
+        <?php
+            }
+        ?>
+    ]
+}
