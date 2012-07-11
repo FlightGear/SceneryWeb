051f5b74c634744fb63bafb2fd4c50924cf4dc3c
OliverJ <acomv41@yahoo.com>
2012-07-11 11:59:47 +0200

    Trying to deal with (potential) concurrent accesses.

--- WWW/scenemodels/submission/static/static_submission.php
+++ WWW/scenemodels/submission/static/static_submission.php
@@ -517,7 +517,7 @@ else {
             // Sending the directory as parameter. This is no user input, so low risk.
             $based64_target_path = base64_encode($target_path);
             ?>
-            <?php include 'get_texture_from_dir.php?p=.$based64_target_path'; ?>
+            <?php include 'get_texture_from_dir.php'; ?>
             </center>
         </td>
     </tr>
