ab5dfa6c889ed28b76c2805ce688faabe170b169
OliverJ <acomv41@yahoo.com>
2012-07-11 11:22:14 +0200

    Trying to deal with (potential) concurrent accesses.

--- WWW/scenemodels/submission/static/get_texture_from_dir.php
+++ WWW/scenemodels/submission/static/get_texture_from_dir.php
@@ -6,12 +6,15 @@ require_once ('../../inc/functions.inc.php');
 // The goal of this small file is to display the texture of a pending model request in the fgs_position_requests table.
 // There is no other (known ;-) possibility to include this in the rest of the static submission script.
 
+// Retrieving directory from parameter sent in URL.
+$target_path = base64_decode($_GET["p"]);
+
 header("Content-type: image/png");
-$dir = opendir("/tmp/submission");
+$dir = opendir($target_path);
 
     while ($file = readdir($dir)) {
         if (ShowFileExtension($file) == "png") {
-            $fichier = '/tmp/submission/'.$file;
+            $fichier = $target_path.$file;
             $texture = imagecreatefrompng($fichier);
             imagepng($texture);
             imagedestroy($texture);
--- WWW/scenemodels/submission/static/static_submission.php
+++ WWW/scenemodels/submission/static/static_submission.php
@@ -513,11 +513,11 @@ else {
         <td>Corresponding PNG Texture Files<br />(click on the pictures to get them bigger)</td>
         <td>
             <center>
-                <?php
-                // Sending the directory as parameter. This is no user input, so low risk.
-                $based64_target_path = base64_encode($target_path);
-                ?>
-                <img src="get_texture_from_dir.php?<?php echo $based64_target_path; ?>">
+            <?php
+            // Sending the directory as parameter. This is no user input, so low risk.
+            $based64_target_path = base64_encode($target_path);
+            ?>
+            <img src="get_texture_from_dir.php?p=<?php echo $based64_target_path; ?>">
             </center>
         </td>
     </tr>
