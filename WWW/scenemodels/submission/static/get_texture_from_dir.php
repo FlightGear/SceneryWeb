0f79afa8070b2d39715bb6ef6012320cd81f6e7a
OliverJ <acomv41@yahoo.com>
2012-07-11 13:42:45 +0200

    Trying to deal with (potential) concurrent accesses.

--- WWW/scenemodels/submission/static/get_texture_from_dir.php
+++ WWW/scenemodels/submission/static/get_texture_from_dir.php
@@ -8,15 +8,15 @@ require_once ('../../inc/functions.inc.php');
 
 // Retrieving directory from parameter sent in URL.
 
-echo "p: ".$_GET["mo_sig"];
-$target_path = base64_decode(rawurldecode($_GET["mo_sig"]));
-echo "target  path: ".$target_path;
-//header("Content-type: image/png");
+// echo "p: ".$_GET["mo_sig"];
+//$target_path = base64_decode(rawurldecode($_GET["mo_sig"]));
+// echo "target  path: ".$target_path;
+header("Content-type: image/png");
 $dir = opendir($target_path);
 
     while ($file = readdir($dir)) {
         if (ShowFileExtension($file) == "png") {
-            $fichier = $target_path.$file;
+            $fichier = $target_path."/".$file;
             $texture = imagecreatefrompng($fichier);
             imagepng($texture);
             imagedestroy($texture);
