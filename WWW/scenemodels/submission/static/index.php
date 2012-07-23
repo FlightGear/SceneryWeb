03cbf79a2c609359daca4bc47e6c27e5ff854b0f
OliverJ <acomv41@yahoo.com>
2012-07-23 15:31:19 +0200

    Updating static index.php following Martin's comments.

--- WWW/scenemodels/submission/static/index.php
+++ WWW/scenemodels/submission/static/index.php
@@ -18,15 +18,14 @@ Please, read the following:
 
     <li>Choose the correct family for your model: static if the model exists only once in the world (eg: Eiffel Tower) or a logical shared family if it can be used elsewhere).</li>
     <li>Choose the author for the model. Please contact us if you're not listed here. If you are building a new model based on another one, put the original creator's name here, and yours in the contributor field.</li>
-    <li>An email address is important: the server will keep you up to date about your submission: submitted, rejected, added, you'll know all of it in real time.</li>
     <li>The country is the one where the model you're adding is located, not yours!</li>
     <li>The description is very important. Has to be short and complete, it will appear in the "name" field for instance <a href="http://scenemodels.flightgear.org/modeledit.php?id=2551">here</a> as well as on the maps.</li>
     <li>Use the terrain shipped with FlightGear/Terrasync, and not any custom elevation model you may have installed/compiled, or model will be sunk/floating.</li>
     <li>The comment is important too, you can be a bit more talkative on your model (not pages!). It appears as "Comment" <a href="http://scenemodels.flightgear.org/modeledit.php?id=2319">here</a>, so don't just say: please commit!</li>
-    <li>You HAVE TO add at least 3 files: an AC3D file of your model, a XML file, and a JPEG thumbnail (PNG texture(s) if any. Files have to share a common name, for instance Rochester_Castle_Keep.ac, Rochester_Castle_Keep.xml, Rochester_Castle_Keep_thumbnail.jpg (thumbnail written as is)). If you have multiple textures, name them Rochester_Castle_Keep1.png, with the number incrementing.</li>
-    <li>JPEG has to be 320*240 exciting thumbnail.</li>
+    <li>You HAVE TO add at least 2 files: an AC3D file of your model and a JPEG thumbnail (PNG texture(s) and XML file if any). Files have to share a common name, for instance Rochester_Castle_Keep.ac, Rochester_Castle_Keep.xml, Rochester_Castle_Keep_thumbnail.jpg (thumbnail written as is)). If you have multiple textures, name them Rochester_Castle_Keep1.png, with an increasing figure.</li>
+    <li>JPEG has to be a 320*240 exciting thumbnail.</li>
     <li>PNG size must be a power of 2 in width and height.</li>
-    <li>XML file must start with a classic XML header, such as: &lt;?xml version="1.0" encoding="UTF-8" ?&gt;</li>
+    <li>XML file must start with a classic XML header, such as: &lt;?xml version="1.0" encoding="UTF-8" ?&gt;. Only send XML if necessary to the model, as it has a performance impact.</li>
     <li>Please also read <a href="http://scenemodels.flightgear.org/contribute.php">this page</a> in order to understand what recommandations this script is looking for.</li>
     <li>Please note that all fields are now mandatory. An XML file is mandatory, as the name of your submission is automagically derived from it and as it should
     be useful for future work. See <a href="TheNameOfYourACFile.xml">here</a> for a quick example.</li>
@@ -75,22 +74,14 @@ Please, read the following:
         </tr>
         <tr>
             <td>
-            <span title="If you did not make the 3D model yourself, but were granted to do so, then you're a contributor. Please enter your name here.">
+            <span title="If you did not make the 3D model yourself, but were granted to do so, then you're a contributor. Please select your name here.">
             <a style="cursor: help; ">Contributor</a>
             </span>
             </td>
             <td>
-            <input type="text" name="contributor" maxlength="100" size="40" value="Your name" onblur="checkStringNotDefault(this,'Your name');checkName(this)"/>
-            </td>
-        </tr>
-        <tr>
-            <td>
-            <span title="Please leave YOUR VALID email address over here. This will help you be informed of your submission process. EXPERIMENTAL">
-            <a style="cursor: help; ">Email address</a>
-            </span>
-            </td>
-            <td>
-            <input type="text" name="email" maxlength="100" size="40" value="Your email" onblur="checkEmail(this);" />
+            <select name="contributor">
+            <?php list_authors(); ?>
+            </select>
             </td>
         </tr>
         <tr>
