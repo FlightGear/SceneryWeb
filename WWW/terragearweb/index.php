3f9d897cc7d39b75e582b50f23f3981b87cd1a5b
OliverJ <acomv41@yahoo.com>
2012-08-16 11:55:46 +0200

    Trying to write down things for this new project.

---
+++ WWW/terragearweb/index.php
@@ -0,0 +1,8 @@
+Hi,
+The purpose of this project is to kind of port the Terragear GUI you may know into a web interface.
+The goal is to help people test their .dat files and get a terrain download links downwards, so they can then submit it to Robin Peel.
+Will take some time, but now that most of the scenemodels is pretty working (still, there are some points to close), I think it's
+time to move forward.
+I also hope this will limit the people needing to install TG on their private machine and use common resources instead, therefore
+willing to participate a bit more to the core scenery more than on private stuff.
+Hopefully, Gijs did a great job on TerraGearGUI, so I hope we can paste quite a lot of his work into this project to avoid reinventing the wheel.
