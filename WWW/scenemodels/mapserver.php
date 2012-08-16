97334af607c921f63e934e5408e81fd9c7af3481
OliverJ <acomv41@yahoo.com>
2012-08-16 14:44:32 +0200

    Inserting an object in better.

--- WWW/scenemodels/mapserver.php
+++ WWW/scenemodels/mapserver.php
@@ -5,7 +5,7 @@ include 'inc/header.php';
 ?>
 <br />
 <center>
-<object data="http://mapserver.flightgear.org/" type="text/html" width="800" height="900"></object>
+<object data="http://mapserver.flightgear.org/" type="text/html" width="800" height="1500"></object>
 </center>
 <?php
 include('inc/footer.php');
