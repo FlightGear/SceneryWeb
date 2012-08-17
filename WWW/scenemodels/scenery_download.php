1ddfb54541b110f34c3bc3e698d14425fb55bdfd
Martin Spott <Martin.Spott@mgras.net>
2012-08-18 00:56:19 +0200

    Scenery version number bump

--- WWW/scenemodels/scenery_download.php
+++ WWW/scenemodels/scenery_download.php
@@ -1,10 +1,10 @@
 <?php
-$page_title = "FlightGear World Scenery v2.6.0";
+$page_title = "FlightGear World Scenery v2.8.0";
 include 'inc/header.php';
 ?>
 <br />
 <center>
-<object data="http://www.flightgear.org/legacy-Downloads/scenery-2.6.0.html" type="text/html" width="800" height="600"></object>
+<object data="http://www.flightgear.org/legacy-Downloads/scenery-2.8.0.html" type="text/html" width="800" height="600"></object>
 </center>
 <?php
 include('inc/footer.php');
