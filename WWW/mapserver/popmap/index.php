47e05ef762917d9591f73361dd6aa5bbe97d6ad5
OliverJ <acomv41@yahoo.com>
2012-09-03 15:52:36 +0200

    Trying to enable static position update.

--- WWW/mapserver/popmap/index.php
+++ WWW/mapserver/popmap/index.php
@@ -71,14 +71,16 @@
 
         function onFeatureSelect(evt) {
             feature = evt.feature;
-            
+
             var content = "<h2>"+feature.attributes.title + "</h2>" +
                 feature.attributes.description;
             if(feature.attributes.type=="shared") {
                 content += "<br/ ><a href='http://scenemodels.flightgear.org/submission/shared/check_update_shared.php?update_choice="+feature.attributes.id+"' target='_blank'>Update</a>"+
                 "&nbsp;<a href='http://scenemodels.flightgear.org/submission/shared/check_delete_shared.php?delete_choice="+feature.attributes.id+"' target='_blank'>Delete</a>";
             }
-            
+            if(feature.attributes.type=="static") {
+                content += "<br/ ><a href='http://scenemodels.flightgear.org/submission/shared/check_update_shared.php?update_choice="+feature.attributes.id+"' target='_blank'>Update</a>";
+            }
             popup = new OpenLayers.Popup.FramedCloud("featurePopup",
                 feature.geometry.getBounds().getCenterLonLat(),
                 new OpenLayers.Size(100,100),
