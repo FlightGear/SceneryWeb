b18207d4e8c7a60e53636c30e17534f589045044
Julien Nguyen <jnguyen@etu.emse.fr>
2012-09-04 08:58:09 +0200

    Adding automatic country computation

---
+++ WWW/scenemodels/inc/country_xml.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This script creates an xml file containing the country code according
+ *
+ */
+
+
+// Inserting libs
+require_once('functions.inc.php');
+
+$long=pg_escape_string($_GET['lg']);
+$lat=pg_escape_string($_GET['lt']);
+
+header('Content-Type: text/xml');
+echo "<?xml version=\"1.0\" standalone=\"yes\" ?>\n";
+echo "<country>";
+compute_country_code_from_position($long, $lat);
+echo "</country>";
+
+?>
