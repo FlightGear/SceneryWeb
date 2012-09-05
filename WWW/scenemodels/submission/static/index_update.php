caf64099b0d0557bb21a87f7516a0a6283af0f41
OliverJ <acomv41@yahoo.com>
2012-09-05 17:47:54 +0200

    First version of index_update for static models.

--- WWW/scenemodels/submission/static/index_update.php
+++ WWW/scenemodels/submission/static/index_update.php
@@ -12,45 +12,32 @@ function validateForm()
 {
     var form = document.getElementById("positions");
 
-    if (!checkNumeric(form["longitude"],-180,180) ||
-        !checkNumeric(form["latitude"],-90,90) ||
-        !checkComment(form["mo_name"]) ||
-        !checkNumeric(form["gndelev"],-10000,10000) ||
-        !checkNumeric(form["offset"],-10000,10000) ||
-        !checkNumeric(form["heading"],0,359.999) ||
-        !checkComment(form["comment"]))
+    if (!checkComment(form["comment"])
         return false;
 
 }
 /*]]>*/
 </script>
 
-<h1>Models Automated Submission Form</h1>
+<h1>Models Automated Update Form</h1>
 
 <p class="center">
-<b>Foreword:</b> This automated form goal is to ease the submission of static and shared 3D models into FG Scenery database.
+<b>Foreword:</b> This automated form goal is to ease the update of static and shared 3D models into FG Scenery database.
 There are currently <?php $models = count_models(); echo number_format($models, '0', '', ' '); ?> models in <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>/models.php">our database</a>.
 Help us to make it more!
 Please, read the following:
 <ul class="warning">
-
-    <li>Choose the correct family for your model: static if the model exists only once in the world (eg: Eiffel Tower) or a logical shared family (if it can be used elsewhere).</li>
-    <li>Choose the author for the model. Please contact us if you're not listed here. If you are building a new model based on another one, put the your name here, and the real author's one into the 'yours in the "Description" field.</li>
-    <li>The country is the one where the model you're adding is located, not yours!</li>
-    <li>The description is very important. Has to be short and complete, it will appear in the "name" field for instance <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>/modelview.php?id=2551">here</a> as well as on the maps.</li>
-    <li>Use the terrain shipped with FlightGear/Terrasync, and not any custom elevation model you may have installed/compiled, or model will be sunk/floating.</li>
-    <li>The comment is important too, you can be a bit more talkative on your model (not pages!). It appears as "Comment" <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>/modelview.php?id=2319">here</a>, so don't just say: please commit!</li>
     <li>You HAVE TO add at least 2 files: an AC3D file of your model and a JPEG thumbnail (PNG texture(s) and XML file if any). Files have to share a common name, for instance Rochester_Castle_Keep.ac, Rochester_Castle_Keep.xml, Rochester_Castle_Keep_thumbnail.jpg (thumbnail written as is)). If you have multiple textures, name them Rochester_Castle_Keep1.png, with an increasing figure.</li>
     <li>JPEG has to be a 320*240 exciting thumbnail.</li>
     <li>PNG size must be a power of 2 in width and height.</li>
     <li>XML file must start with a classic XML header, such as: &lt;?xml version="1.0" encoding="UTF-8" ?&gt;. See <a href="TheNameOfYourACFile.xml">here</a> for a quick example. Only send XML if necessary to the model, as it has a performance impact.</li>
     <li>Please also read <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>/contribute.php">this page</a> in order to understand what recommandations this script is looking for.</li>
-    <li>Do not try to import/update an already existing model: there will be an update script [when we have some spare time].</li>
     <li>Don't forget to feed the Captcha, it's a mandatory item as well. Don't know what a Captcha is or what its goal is? Learn more <a href="http://en.wikipedia.org/wiki/Captcha">here</a></li>
+    <li>In this form, you need to add ALL files of the model, even if all are not updated.</li>
     <li>I think that's all, folks ;-) Ah yes, be patient, there are human beings with real life constraints behind, and don't feel blamed if your models are rejected, but try to understand why.</li>
 </ul>
 </p>
-    <form id="positions" method="post" action="check_static.php" enctype="multipart/form-data" onsubmit="return validateForm();">
+    <form id="positions" method="post" action="check_update_static.php" enctype="multipart/form-data" onsubmit="return validateForm();">
     <table>
         <tr>
             <td>
@@ -72,100 +59,6 @@ Please, read the following:
             pg_close ($resource_r);
             ?>
             </select>
-            </td>
-        </tr>
-        <tr>
-            <td>
-            <span title="This is the name of the author. If the author does not exist, please ask the scenery maintainers to add it. This name is the author of the true creator of the model, if you just converted a model and were granted to do so, then also use the line below.">
-            <label for="mo_author">Author</label>
-            </span>
-            </td>
-            <td>
-            <select name="mo_author" id="mo_author">
-            <?php list_authors(); ?>
-            </select>
-        </td>
-        </tr>
-        <tr>
-            <td>
-            <span title="This is the country code where the model is located (for static models only).">
-            <label for="ob_country">Country</label>
-            </span>
-            </td>
-            <td>
-            <select name="ob_country" id="ob_country">
-            <?php list_countries(); ?>
-            </select>
-            </td>
-        </tr>
-        <tr>
-            <td>
-            <span title="Please add a short (max 100 letters) name of your model (eg : Cornet antenna radome - Brittany - France).">
-            <label for="mo_name">Description</label>
-            </span>
-            </td>
-            <td>
-            <input type="text" name="mo_name" id="mo_name" maxlength="100" size="40" value="Tell us more about your model." onchange="checkComment(this);"/>
-            </td>
-        </tr>
-        <tr>
-            <td>
-            <span title="Please add a short statement giving more details on this data. eg: The Cite des Telecoms, colocated with the cornet radome, is a telecommunications museum.">
-            <label for="comment">Comment</label>
-            </span>
-            </td>
-            <td>
-            <input type="text" name="comment" id="comment" maxlength="500" size="40" value="" onchange="checkComment(this);" />
-            </td>
-        </tr>
-        <tr>
-            <td>
-            <span title="This is the WGS84 longitude of the object you want to add. Has to be between -180.000000 and +180.000000.">
-            <label for="longitude">Longitude</label>
-            </span>
-            </td>
-            <td>
-            <input type="text" name="longitude" id="longitude" maxlength="11" value="" onchange="checkNumeric(this,-180,180);" />
-            </td>
-        </tr>
-        <tr>
-            <td>
-            <span title="This is the WGS84 latitude of the object you want to add. Has to be between -90.000000 and +90.000000.">
-            <label for="latitude">Latitude</label>
-            </span>
-            </td>
-            <td>
-            <input type="text" name="latitude" id="latitude" maxlength="10" value="" onchange="checkNumeric(this,-90,90);" />
-            </td>
-        </tr>
-        <tr>
-            <td>
-            <span title="This is the ground elevation (in meters) of the position where the object you want to add is located. Warning: if your object is sunk into the ground, use the elevation offset field below.">
-            <label for="gndelev">Elevation</label>
-            </span>
-            </td>
-            <td>
-            <input type="text" name="gndelev" id="gndelev" maxlength="10" value="" onchange="checkNumeric(this,-10000,10000);" />
-            </td>
-        </tr>
-        <tr>
-            <td>
-            <span title="This is the offset (in meters) between your object 'zero' and the elevation at the considered place (ie if it is sunk into the ground). Let 0 if there is no offset.">
-            <label for="offset">Elevation offset</label> (see <a href="../../contribute.php#offset">here</a> for more help)
-            </span>
-            </td>
-            <td>
-            <input type="text" name="offset" id="offset" maxlength="10" value="0" onchange="checkNumeric(this,-10000,10000);" />
-            </td>
-        </tr>
-        <tr>
-            <td>
-            <span title="The orientation (in degrees) of the object you want to add - as it appears in the .stg file (this is NOT the true heading). Let 0 if there is no specific orientation.">
-            <label for="heading">Orientation</label>
-            </span>
-            </td>
-            <td>
-            <input type="text" name="heading" id="heading" maxlength="7" value="" onchange="checkNumeric(this,0,359.999);" />
             </td>
         </tr>
         <tr>
