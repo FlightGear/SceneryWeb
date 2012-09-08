8b3dc0f82cbd98ce62accfe6700dcd6980926681
Martin Spott <Martin.Spott@mgras.net>
2012-09-08 19:45:33 +0200

    Further adaptions to missing Telascience DNS

--- WWW/mapserver/map/maplayers.js
+++ WWW/mapserver/map/maplayers.js
@@ -42,56 +42,39 @@
 //    );
 
     var customscene = new OpenLayers.Layer.WMS( "Landcover-DB CS Test",
-	    [ "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
-	      "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&" ],
+	    "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
 	    {layers: 'cs_herbtundra,cs_heath,cs_barrencover,cs_dirt,cs_rainforest,cs_mixedforest,cs_evergreenforest,cs_deciduousforest,cs_olives,cs_orchard,cs_vineyard,cs_burnt,cs_sclerophyllous,cs_scrub,cs_greenspace,cs_grassland,cs_golfcourse,cs_agroforest,cs_cropgrass,cs_naturalcrop,cs_complexcrop,cs_mixedcrop,cs_rice,cs_irrcrop,cs_drycrop,cs_cemetery,cs_transport,cs_construction,cs_industrial,cs_dump,cs_openmining,cs_lava,cs_rock,cs_saltmarsh,cs_littoral,cs_saline,cs_sand,cs_marsh,cs_bog,cs_estuary,cs_lagoon,cs_floodland,cs_town,cs_suburban,cs_port,cs_urban,cs_polarice,cs_packice,cs_glacier,cs_watercourse,cs_intermittentlake,cs_lake,cs_asphalt,cs_airport', format: 'image/png'},
         {minZoomLevel: 10}
     );
 
 //    var customscene = new OpenLayers.Layer.WMS( "Landcover-DB CS Test",
-//	      [ "http://mapserver.flightgear.org/tc?",
-//	        "http://mapserver.flightgear.org/tc?",
-//	        "http://mapserver.flightgear.org/tc?",
-//	        "http://mapserver.flightgear.org/tc?" ],
+//	      "http://mapserver.flightgear.org/tc?",
 //	      {layers: 'customscene', format: 'image/png'}
 //    );
 
     var csdefault = new OpenLayers.Layer.WMS( "Landcover-DB CS Voidfill",
-	    [ "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
-	      "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&" ],
+	    "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
 	    {layers: 'cs_default', transparent: 'true', format: 'image/png'},
 	    {isBaseLayer: false}
     );
 
     var v0cover = new OpenLayers.Layer.WMS( "Landcover-DB VMap0-Vector",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'v0cover', format: 'image/png'}
 	);
 
 	var clc00 = new OpenLayers.Layer.WMS( "Landcover-DB CLC2000v15",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'clc00', format: 'image/png'}
 	);
 
 	var clc06 = new OpenLayers.Layer.WMS( "Landcover-DB CLC2006v15",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'clc06', format: 'image/png'}
 	);
 
 	var icubed = new OpenLayers.Layer.WMS( "TelaScience i-Cubed", 
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'icubed', format: 'image/png'},
 	    {wrapDateLine: true}
 	);
@@ -111,19 +94,13 @@
     );
 
     var tarmac = new OpenLayers.Layer.WMS( "Airfield layouts (polygon)",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'tarmac', transparent: 'true', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
 
     var tarmac850 = new OpenLayers.Layer.WMS( "v8.50 Airfield layouts (polygon)",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'tarmac850', transparent: 'true', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
@@ -135,118 +112,88 @@
 //    );
 
     var navaid850 = new OpenLayers.Layer.WMS( "v8.50 Navaids (symbol)",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'navaid850', transparent: 'true', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
 
     var osmlines = new OpenLayers.Layer.WMS( "OSM line features",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'osmlines', transparent: 'true', format: 'image/png'},
 	    {isBaseLayer: false}
 //    {gutter: 50}
     );
 
     var osmtarmac = new OpenLayers.Layer.WMS( "OSM Airfield layouts (line)",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'osmtarmac', transparent: 'true', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
 
     var cslines = new OpenLayers.Layer.WMS( "VMap0/CS line features",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'cslines', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
 
 //    var cslines = new OpenLayers.Layer.WMS( "VMap0/CS line features",
-//	      [ "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
-//	        "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&" ],
+//	      "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
 //	      {layers: 'cs_freeway,cs_road,cs_railroad2,cs_railroad1,cs_canal,cs_intermittentstream,cs_stream', format: 'image/png'},
 //	      {isBaseLayer: false}
 //    );
 
     var noaroads = new OpenLayers.Layer.WMS( "StatsCan/TIGER roads (line)",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'noaroads', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
 
     var airfield = new OpenLayers.Layer.WMS( "Airfields (point)",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'airfield', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
 
     var airport850 = new OpenLayers.Layer.WMS( "v8.50 Airfields (symbol)",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'airport850', transparent: 'true', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
 
     var sceneobject = new OpenLayers.Layer.WMS( "Scenery Objects (point)",
-	    [ "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
-	      "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&" ],
+	    "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
 	    {layers: 'fgs_staticobjects,fgs_sharedobjects', transparent: 'true', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
 
 //    var sceneobject = new OpenLayers.Layer.WMS( "Scenery Objects (point)",
-//	      [ "http://mapserver.flightgear.org/tc?",
-//	        "http://mapserver.flightgear.org/tc?",
-//	        "http://mapserver.flightgear.org/tc?",
-//	        "http://mapserver.flightgear.org/tc?" ],
+//	      "http://mapserver.flightgear.org/tc?",
 //	      {layers: 'sceneobject', transparent: 'true', format: 'image/png'},
 //	      {isBaseLayer: false}
 //    );
 
     var wmssigns = new OpenLayers.Layer.WMS( "WMS Taxiway Signs",
-        [ "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
-          "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&" ],
+        "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
         {layers: 'fgs_signs', transparent: 'true', format: 'image/png'},
         {isBaseLayer: false, maxScale: 5000 }
     );
 
     //TODO This is temporary code    
     var wfssigns = new OpenLayers.Layer.WMS( "WMS Taxiway Signs",
-        [ "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
-          "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&" ],
+        "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
         {layers: 'fgs_signs', transparent: 'true', format: 'image/png'},
         {isBaseLayer: false, minScale: 5000 }
     );
 
 // This code is buggy
 //    var wfssigns = new OpenLayers.Layer.WFS( "WFS Taxiway Signs",
-//        [ "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
-//          "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&" ],
+//        "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
 //        { typename: "fgs_signs", maxfeatures: 200},
 //        { minScale: 5000 }
 //    );
 
     var wmsobjects = new OpenLayers.Layer.WMS( "Scenery Objects",
-        [ "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
-          "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&" ],
+        "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
         {layers: 'fgs_staticobjects,fgs_sharedobjects', transparent: 'true', format: 'image/png'},
         {isBaseLayer: false, maxScale: 12500 }
     );
@@ -271,35 +218,25 @@
     );
 
     var gshhs = new OpenLayers.Layer.WMS( "GSHHS coastline (polygon)",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'gshhs', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
 
     var swbd = new OpenLayers.Layer.WMS( "SWBD coastline (polygon)",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'swbd', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
 
     var fgbuckets = new OpenLayers.Layer.WMS( "FG Scenery buckets",
-	    [ "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?",
-	      "http://mapserver.flightgear.org/tc?" ],
+	    "http://mapserver.flightgear.org/tc?",
 	    {layers: 'fgbuckets', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
 
     var osmlinecover = new OpenLayers.Layer.WMS( "OSM line features (polygon, experimental)",
-	    [ "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
-	      "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&" ],
+	    "http://mapserver.flightgear.org/ms?srs=EPSG%3A900913&",
 	    {layers: 'osm_roadcover', format: 'image/png'},
 	    {isBaseLayer: false}
 	);
