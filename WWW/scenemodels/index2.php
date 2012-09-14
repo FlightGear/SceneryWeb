45b33605764bd634686f7eede7f2c1c29769e237
Julien Nguyen <jnguyen@etu.emse.fr>
2012-09-14 08:38:43 +0200

    Test for new main page

---
+++ WWW/scenemodels/index2.php
@@ -0,0 +1,76 @@
+<?php include 'inc/header.php';?>
+<?php
+    if (isset($_REQUEST['offset']) && preg_match('/^[0-9]+$/u', $_REQUEST['offset'])) {
+        $offset = $_REQUEST['offset'];
+    }
+    else {
+        $offset = 0;
+    }
+?>
+
+  <h1>FlightGear Scenery Database</h1>
+  
+  
+  <table class="float">
+      <tr><th colspan="2">Recently updated objects</th></tr>
+<?php
+        $query = "SELECT ob_id, ob_text, to_char(ob_modified,'YYYY-mm-dd (HH24:MI)') AS ob_datedisplay " .
+                 "FROM fgs_objects " .
+                 "ORDER BY ob_modified DESC " .
+                 "LIMIT 10";
+        $result = pg_query($query);
+        while ($row = pg_fetch_assoc($result)) {
+            echo "<tr>\n" .
+                    "<td><a href=\"objectview.php?id=".$row["ob_id"]."\">".$row["ob_text"]."</a></td>\n" .
+                    "<td>".$row["ob_datedisplay"]."</td>\n" .
+                 "</tr>\n";
+        }
+?>
+  </table>
+  <table class="float">
+      <tr><th colspan="2">Recently updated models</th></tr>
+<?php
+        $query = "SELECT mo_id, mo_name, to_char(mo_modified,'YYYY-mm-dd (HH24:MI)') AS mo_datedisplay " .
+                 "FROM fgs_models " .
+                 "ORDER BY mo_modified DESC " .
+                 "LIMIT 10";
+        $result = pg_query($query);
+        while ($row = pg_fetch_assoc($result)){
+            echo "<tr>\n" .
+                    "<td><a href=\"modelview.php?id=".$row["mo_id"]."\">".$row["mo_name"]."</a></td>\n" .
+                    "<td>".$row["mo_datedisplay"]."</td>\n" .
+                "</tr>\n";
+        }
+?>
+  </table>
+  <div class="clear"></div><br/>
+  
+  <table>
+    <tr><th colspan="2">Latest news</th></tr>
+<?php
+    $query = "SELECT *, date_trunc('seconds',ne_timestamp) AS formdate ";
+    $query.= "FROM fgs_news, fgs_authors ";
+    $query.= "WHERE au_id = ne_author ";
+    $query.= "ORDER BY ne_timestamp DESC ";
+    $query.= "LIMIT 5 OFFSET ".$offset;
+    $result = pg_query($query);
+    while ($row = pg_fetch_assoc($result)) {
+        echo "<tr><td>\n" .
+             "<div class=\"newsdate\">".$row["formdate"]."</div>\n" .
+             "<div class=\"newsnormal\">by</div><div class=\"newsauthor\"><a href=\"author.php?id=".$row["au_id"]."\">".$row["au_name"]."</a></div><div class=\"clear\"></div><hr/>\n" .
+             "".$row["ne_text"]."</td></tr>\n";
+    }
+?>
+
+    <tr class="bottom">
+        <td colspan="9" align="center">
+            <?php 
+            if ($offset >= 5) {
+                echo "<a href=\"index.php?offset=".($offset-5)."\">&lt; Newer news</a> | ";
+            }
+            ?>
+            <a href="index.php?offset=<?php echo $offset+5;?>">Older news &gt;</a>
+        </td>
+    </tr>
+  </table>
+<?php include 'inc/footer.php';?>
