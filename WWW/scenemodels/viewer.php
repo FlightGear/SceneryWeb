303809fc674287209c2ed0bbefbc2a10fa63e2be
Gijs de Rooy <gijsrooy@hotmail.com>
2012-10-26 13:25:03 +0200

    Hehe, finally a nice solution for checking WebGL availability. Should throw an error when WebGL is supported, but disabled now.

--- WWW/scenemodels/viewer.php
+++ WWW/scenemodels/viewer.php
@@ -1,87 +1,104 @@
 <html>
 <head>
-<style type="text/css">
+    <style type="text/css">
+        #canvas {
+            width: 570px;
+            cursor:move;
+            z-index: 10;
+        }
 
-#canvas {
-  width: 570px;
-  cursor:move;
-  z-index: 10;
-}
+        #loading {
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+            display: none;
+            background: url('loader.gif') no-repeat center center;
+            z-index: 100;
+        }
+    </style>
 
-#loading {
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  display: none;
-  background: url('loader.gif') no-repeat center center;
-  z-index: 100;
-}
-</style>
-<script type="text/javascript" src="inc/hangar/gl-matrix-min.js"></script>
-<script type="text/javascript" src="inc/hangar/polyfill.js"></script>
-<script type="text/javascript" src="inc/hangar/viewer.js"></script>
+    <script type="text/javascript" src="inc/hangar/gl-matrix-min.js"></script>
+    <script type="text/javascript" src="inc/hangar/polyfill.js"></script>
+    <script type="text/javascript" src="inc/hangar/viewer.js"></script>
 
-<?php
-if (isset($_REQUEST['id']) && (preg_match('/^[0-9]+$/u',$_GET['id']))) {
-    $id = $_REQUEST['id'];
-}
-?>
+    <?php
+    if (isset($_REQUEST['id']) && (preg_match('/^[0-9]+$/u',$_GET['id']))) {
+        $id = $_REQUEST['id'];
+    }
+    ?>
 
-<script type="text/javascript">
+    <script type="text/javascript">
+    function $$(x) {
+        return document.getElementById(x);
+    }
 
+    var Models = [
+        { file: "get_ac3d_from_dir.php?id=<?php echo rawurlencode($id); ?>"}
+    ];
 
+    var canvas, details, loading, viewer, current, gl;
 
-var Models = [
-  { file: "get_ac3d_from_dir.php?id=<?php echo rawurlencode($id); ?>"}
-];
+    function launchViewer() {
+        details = document.getElementById("details");
+        loading = document.getElementById("loading");
+        viewer = new HG.Viewer(canvas);
+        current = 0;
 
-var canvas, details, loading, viewer, current;
+        resize();
+        showModel(Models[current]);
+    }
 
-function onLoad(){
-  canvas = document.getElementById("canvas");
+    function onLoad() {
+        canvas = document.getElementById("canvas");
+        var experimental = false;
+        try { gl = canvas.getContext("webgl"); }
+        catch (x) { gl = null; }
 
-  // check if the browser support WebGL
-  if (!window.WebGLRenderingContext) {
-       window.location = "http://get.webgl.org";
-  } else {
-    details = document.getElementById("details");
-    loading = document.getElementById("loading");
-    viewer = new HG.Viewer(canvas);
-    current = 0;
+        if (gl == null) {
+            try { gl = canvas.getContext("experimental-webgl"); experimental = true; }
+            catch (x) { gl = null; }
+        }
 
-    resize();
-    showModel(Models[current]);
-  }
-};
+        if (gl) {
+            // WebGL is supported and enabled
+            launchViewer();
+        } else if ("WebGLRenderingContext" in window) {
+            // WebGL is supported, but not enabled
+            window.location = "http://get.webgl.org";
+        } else {
+            // WebGL is not supported
+            window.location = "http://get.webgl.org";
+        }
+    }
 
-function resize(){
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+    function resize(){
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
 
-  window.addEventListener("resize",
-    function(event){
-      viewer.onResize(window.innerWidth, window.innerHeight);
-    }, false);
-};
+      window.addEventListener("resize",
+        function(event){
+          viewer.onResize(window.innerWidth, window.innerHeight);
+        }, false);
+    };
 
-function showModel(model){
-  loading.style.display = "block";
-  viewer.show(model.file, {callback:onLoaded, texturePath:"get_texture_by_filename.php?id=<?php echo rawurlencode($id); ?>&name="});
-};
+    function showModel(model){
+        loading.style.display = "block";
+        viewer.show(model.file, {callback:onLoaded, texturePath:"get_texture_by_filename.php?id=<?php echo rawurlencode($id); ?>&name="});
+    };
 
-function onLoaded(){
-  loading.style.display = "none";
-};
-</script>
+    function onLoaded(){
+        loading.style.display = "none";
+    };
+    </script>
 
 </head>
 
 <body onload="onLoad();">
-  <canvas id="canvas"></canvas>
-  <div id="loading"></div>
+    <canvas id="canvas"></canvas>
+    <div id="loading"></div>
 </body>
 
 </html>
