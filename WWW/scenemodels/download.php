3d2718a646ceca6055d7e3c3d951327ba0fd2ad1
Gijs de Rooy <gijsrooy@hotmail.com>
2012-11-08 22:07:21 +0100

    Clarify download page.

--- WWW/scenemodels/download.php
+++ WWW/scenemodels/download.php
@@ -1,21 +1,17 @@
 <?php include 'inc/header.php'; ?>
 <h1>Scenery Downloads</h1>
 <p>
-  In order to have the latest up-to-date scenery, it is <strong>RECOMMENDED</strong> to use <strong>Terrasync</strong> included with FlightGear to download automatically the scenery when you are flying.</p>
+    In order to have the latest up-to-date scenery, it is <strong>RECOMMENDED</strong> to use <strong>TerraSync</strong>, included with FlightGear, to download the scenery automatically when you are flying. More information about TerraSync can be found <a href="http://wiki.flightgear.org/TerraSync">at our wiki</a>.</p>
 <p>
-  However, for any reason, you still can download the files here:
-  <ul>
-    <li><a href="scenery_download.php">Scenery download</a>: contains the scenery ground but <strong>the objects are not up-to-date</strong></li>
-    <li><a href="objects_download.php">Scenery objects download</a>: contains <strong>only</strong> the latest up-to-date objects</li>
-  </ul>
+    However, for any reason, you can still download the files here:
+    <ul>
+        <li><a href="scenery_download.php">Terrain and objects </a>: but <strong>the objects are not up-to-date</strong></li>
+        <li><a href="objects_download.php">Objects</a>: <strong>only</strong> the latest up-to-date objects</li>
+        <ul>
+            <li><a href="/download/GlobalObjects.tgz">Global objects </a>: contains all objects in the world.</li>
+        </ul>
+        <li><a href="/download/SharedModels.tgz">Shared models</a>: eg. windturbines, jetways, trees. This file is <strong>*REQUIRED*</strong> if you want to see all the objects, and should be unpacked in your $FG_ROOT directory.</li>
+    </ul>
 </p>
-<p>
-  Currently a global file is also available <a href="/download/GlobalObjects.tgz">here</a>.
-</p>
-<p>
-  A collection of shared models can be downloaded <a href="/download/SharedModels.tgz">here</a>.<br/>
-  (This file is <strong>*REQUIRED*</strong> if you want to see all the objects, and should be unpacked in your FGROOT directory)
-</p>
-
 
 <?php include 'inc/footer.php'; ?>
