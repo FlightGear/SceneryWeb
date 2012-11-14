c8ceab3b53e15d2c01ae8a75640e30bed3449587
Gijs de Rooy <gijsrooy@hotmail.com>
2012-11-14 13:42:47 +0100

    Draw map when importing via UFO.

--- WWW/scenemodels/submission/shared/index.php
+++ WWW/scenemodels/submission/shared/index.php
@@ -138,10 +138,6 @@ function validateForm()
         <td><span title="This is the WGS84 latitude of the object you want to add. Has to be between -90.000000 and +90.000000."><label for="latitude">Latitude<em>*</em></label></span></td>
         <td>
             <input type="text" name="latitude" id="latitude" maxlength="13" value="<?php echo substr($_GET['lat'],0,13); ?>" onchange="update_map('longitude','latitude');checkNumeric(this,-90,90);" />
-            <?php 
-            if ($ufo)
-                echo "<script>update_map('longitude','latitude');checkNumeric(this,-90,90)</script>";
-            ?>
         </td>
     </tr>
     <tr>
@@ -150,6 +146,10 @@ function validateForm()
         </td>
         <td>
             <object id="map" data="" type="text/html" width="300" height="225"></object>
+            <?php 
+            if ($ufo)
+                echo "<script>update_map('longitude','latitude');checkNumeric(this,-90,90)</script>";
+            ?>
         </td>
     </tr>
     <tr>
