582f324b6569d10953337a79fb5fe87c03d3e761
Gijs de Rooy <gijsrooy@hotmail.com>
2012-12-24 14:38:33 +0100

    Colour field border when numeric is ok/wrong.

--- WWW/scenemodels/submission/shared/tabs_test.php
+++ WWW/scenemodels/submission/shared/tabs_test.php
@@ -36,6 +36,14 @@ function validateForm()
         return false;
 }
 
+function checkNumeric(element, min, max) {
+    if ((element.value > max) && (element.value < max)) {
+        element.style.border = "2px solid rgb(0, 200, 0)";
+    } else {
+        element.style.border = "2px solid rgb(200, 0, 0)";
+    }
+}
+
 function  validateTabs()
 {
     var form = document.getElementById("positions");
