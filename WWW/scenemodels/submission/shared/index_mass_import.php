b9af29ff1e8b84de4c9273cabe0eb875bcf5accf
Gijs de Rooy <gijsrooy@hotmail.com>
2013-01-04 22:20:40 +0100

    Style tooltips/hover

--- WWW/scenemodels/submission/shared/index_mass_import.php
+++ WWW/scenemodels/submission/shared/index_mass_import.php
@@ -86,17 +86,17 @@ function validateForm()
 <form id="positions" method="post" action="confirm_mass_import.php" onsubmit="return validateForm();">
 <table>
     <tr>
-        <td><span title="This is the content of the STG file you want to add."><label for="stg">Content to add<em>*</em></label></span></td>
+        <td><label for="stg">Content to add<em>*</em><span>This is the content of the STG file you want to add.</span></label></td>
         <td><textarea name="stg" id="stg" rows="30" cols="100" onchange="checkSTG(this);"></textarea></td>
     </tr>
     <tr>
-        <td><span title="Please leave YOUR VALID email address over here. This will help you be informed of your submission process."><label for="email">Email address</label></span></td>
+        <td><label for="email">Email address<span>Please leave YOUR VALID email address over here. This will help you be informed of your submission process.></span></label></td>
         <td>
             <input type="text" name="email" id="email" maxlength="50" size="50" value="" onkeyup="checkEmail(this);" />
         </td>
     </tr>
     <tr>
-        <td><span title="Please add a short (max 100 letters) statement why you are inserting this data. This will help the maintainers understand what you are doing. eg: 'I have placed a couple of aircraft shelters and static F16's at EHVK, please commit.' Only alphanumerical, colon, semi colon, question and exclamation mark, arobace, minus, underscore, antislash and point are granted."><label for="comment">Comment<em>*</em></label></span></td>
+        <td><label for="comment">Comment<em>*</em><span>Please add a short (max 100 letters) statement why you are inserting this data. This will help the maintainers understand what you are doing. eg: 'I have placed a couple of aircraft shelters and static F16's at EHVK, please commit.' Only alphanumerical, colon, semi colon, question and exclamation mark, arobace, minus, underscore, antislash and point are granted.</span></label></td>
         <td>
             <input type="text" name="comment" id="comment" maxlength="100" size="100" value="" onkeyup="checkComment(this);" />
             <input name="IPAddr" type="hidden" value="<?php echo $_SERVER['REMOTE_ADDR']?>" />
