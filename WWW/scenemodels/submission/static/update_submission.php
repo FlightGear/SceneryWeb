bf3ef5e6ac1030bfa1ac0a8e53bc463924b16b2f
Gijs de Rooy <gijsrooy@hotmail.com>
2013-01-14 14:23:24 +0100

    Debug

--- WWW/scenemodels/submission/static/update_submission.php
+++ WWW/scenemodels/submission/static/update_submission.php
@@ -216,6 +216,8 @@ if (!isset($_POST["action"])) {
 
             // Gzuncompress the query
             $query_rw = gzuncompress($sqlz);
+			
+			echo $query_rw;
 
             // Retrieve data from query
             $pattern = "/INSERT INTO fgs_models \(mo_id, mo_path, mo_author, mo_name, mo_notes, mo_thumbfile, mo_modelfile, mo_shared\) VALUES \((?P<modelid>[0-9]+), '(?P<path>[a-zA-Z0-9_.-]+)', (?P<author>[0-9]+), '(?P<name>[a-zA-Z0-9,;:?@ !_.-]+)', '(?P<notes>[a-zA-Z0-9 ,!_.-]*)', '(?P<thumbfile>[a-zA-Z0-9=+\/]+)', '(?P<modelfile>[a-zA-Z0-9=+\/]+)', (?P<shared>[0-9]+)\)/";
