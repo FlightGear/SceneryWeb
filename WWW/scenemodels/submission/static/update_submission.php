af906b0f6856c89f745ea2f992204a62abb4789a
Gijs de Rooy <gijsrooy@hotmail.com>
2013-01-14 14:38:32 +0100

    Debug

--- WWW/scenemodels/submission/static/update_submission.php
+++ WWW/scenemodels/submission/static/update_submission.php
@@ -217,6 +217,8 @@ if (!isset($_POST["action"])) {
             // Gzuncompress the query
             $query_rw = gzuncompress($sqlz);
 
+			echo $query_rw;
+			
             // Retrieve data from query
             $pattern = "/UPDATE fgs_models SET mo_path \= '(?P<path>[a-zA-Z0-9_.-]+)', mo_author \= (?P<author>[0-9]+), mo_name \= '(?P<name>[a-zA-Z0-9,;:?@ !_.-]+)', mo_notes \= '(?P<notes>[a-zA-Z0-9 ,!_.-]*)', mo_thumbfile \= (?P<thumbfile>[a-zA-Z0-9=+\/]+)', mo_modelfile \= '(?P<modelfile>[a-zA-Z0-9=+\/]+)', mo_shared \= (?P<shared>[0-9]+) WHERE mo_id \= (?P<modelid>[0-9]+)/";
             $error === preg_match($pattern, $query_rw, $matches);
