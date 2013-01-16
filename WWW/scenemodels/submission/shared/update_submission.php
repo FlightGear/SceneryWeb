56cc654053b2da333b268e4bd659ac63ddba447c
Gijs de Rooy <gijsrooy@hotmail.com>
2013-01-16 16:25:03 +0100

    Fix

--- WWW/scenemodels/submission/shared/update_submission.php
+++ WWW/scenemodels/submission/shared/update_submission.php
@@ -46,20 +46,20 @@
                     echo "<p class=\"center\">Signature found.<br /> Now processing query with request number ". $_GET["sig"].".\n</p>\n";
 					
                     $trigged_query_rw = strstr($query_rw, 'wkb_geometry'); // Removing the start of the query from the data;
+					$trigged_query_rw = str_replace('$',''$trigged_query_rw);
                     
                     echo "<table>\n<tr>\n<th></th>\n<th>Old</th>\n<th>New</th>\n</tr>\n";
                     
-                    $pattern = "/wkb_geometry\=ST_PointFromText\('POINT\((?P<long>[0-9.-]+) (?P<lat>[0-9.-]+)\)', 4326\), ob_gndelev\=(?P<elev>[0-9.-]+), ob_elevoffset\=(?P<elevoffset>(([0-9.-]+)|NULL)), ob_heading\=(?P<orientation>[0-9.-]+), ob_model\=(?P<model_id>[0-9]+), ob_group\=1 WHERE ob_id\=(?P<object_id>[0-9]+)/";
+                    $pattern = "/SET ob_text\=(?P<notes>[a-zA-Z0-9 ,!_.-]*), wkb_geometry\=ST_PointFromText\('POINT\((?P<long>[0-9.-]+) (?P<lat>[0-9.-]+)\)', 4326\), ob_gndelev\=(?P<elev>[0-9.-]+), ob_elevoffset\=(?P<elevoffset>(([0-9.-]+)|NULL)), ob_heading\=(?P<orientation>[0-9.-]+), ob_model\=(?P<model_id>[0-9]+), ob_group\=1 WHERE ob_id\=(?P<object_id>[0-9]+)/";
 					
 					echo $pattern."\r\n\r\n";
 					echo $trigged_query_rw;
                     
                     $error === preg_match($pattern, $trigged_query_rw, $matches);
-					print $error;
 					
 					print_r($matches);
 					
-                    //$notes = $matches['notes'];
+                    $notes = $matches['notes'];
                     $long = $matches['long'];
                     $lat = $matches['lat'];
                     $elev = $matches['elev'];
