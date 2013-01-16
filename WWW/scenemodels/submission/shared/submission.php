e437ea9e0c626108e5b00f6e7910c9c95c55ca56
Gijs de Rooy <gijsrooy@hotmail.com>
2013-01-16 18:43:08 +0100

    Merge normal and update submission.

--- WWW/scenemodels/submission/shared/submission.php
+++ WWW/scenemodels/submission/shared/submission.php
@@ -89,6 +89,115 @@
             }
         }
     }
+    
+    // Check the presence of "action", the presence of "signature", its length (64) and its content.
+    if (isset($_GET["action"]) && isset($_GET["sig"]) && (strlen($_GET["sig"]) == 64) && preg_match("/[0-9a-z]/", $_GET["sig"]) && ($_GET["action"] == "check_update")) {
+        $resource_rw = connect_sphere_rw();
+
+        // If connection is OK
+        if ($resource_rw != '0') {
+
+            // Checking the presence of sig into the database
+            $result = @pg_query($resource_rw, "SELECT spr_hash, spr_base64_sqlz FROM fgs_position_requests WHERE spr_hash = '". $_GET["sig"] ."';");
+            if (pg_num_rows($result) != 1) {
+                $page_title = "Automated Objects Massive Import Request Form";
+                $error_text = "Sorry but the request you are asking for does not exist into the database. Maybe it has already been validated by someone else?<br/>";
+                $advise_text = "Else, please report to devel ML or FG Scenery forum.";
+                include '../../inc/error_page.php';
+                @pg_close($resource_rw);
+                exit;
+            }
+
+            if ($_GET["action"] == "check_update") {  // If action comes from the mass submission script
+                while ($row = pg_fetch_row($result)) {
+                    $sqlzbase64 = $row[1];
+
+                    // Base64 decode the query
+                    $sqlz = base64_decode($sqlzbase64);
+
+                    // Gzuncompress the query
+                    $query_rw = gzuncompress($sqlz);
+                    $page_title = "Automated Objects Massive Import Requests Form";
+                    include '../../inc/header.php';
+                    echo "<p class=\"center\">Signature found.<br /> Now processing query with request number ". $_GET["sig"].".\n</p>\n";
+                    
+                    $trigged_query_rw = strstr($query_rw, 'SET'); // Removing the start of the query from the data;
+                    $trigged_query_rw = str_replace('$','',$trigged_query_rw);
+                    
+                    echo "<table>\n<tr>\n<th></th>\n<th>Old</th>\n<th>New</th>\n</tr>\n";
+                    
+                    $pattern = "/SET ob_text\=(?P<notes>[a-zA-Z0-9 ,!_.-]*), wkb_geometry\=ST_PointFromText\('POINT\((?P<lon>[0-9.-]+) (?P<lat>[0-9.-]+)\)', 4326\), ob_gndelev\=(?P<elev>[0-9.-]+), ob_elevoffset\=(?P<elevoffset>(([0-9.-]+)|NULL)), ob_heading\=(?P<orientation>[0-9.-]+), ob_model\=(?P<model_id>[0-9]+), ob_group\=1 WHERE ob_id\=(?P<object_id>[0-9]+)/";
+                    
+                    $error === preg_match($pattern, $trigged_query_rw, $matches);
+                    
+                    $notes = $matches['notes'];
+                    $lon = $matches['lon'];
+                    $lat = $matches['lat'];
+                    $elev = $matches['elev'];
+                    $elevoffset = $matches['elevoffset'];
+                    $orientation = $matches['orientation'];
+                    // $country = $matches['country'];
+                    $model_id = $matches['model_id'];
+                    $object_id = $matches['object_id'];
+                    
+                    // Obtain old/current values
+                    $result = pg_query("SELECT *, ST_Y(wkb_geometry) AS ob_lat, ST_X(wkb_geometry) AS ob_lon FROM fgs_objects WHERE ob_id=$object_id;");
+                    $object = pg_fetch_assoc($result);
+                    
+                    $lon_old = get_object_longitude_from_id($object_id);
+                    $lat_old = get_object_latitude_from_id($object_id);
+                    $elev_old = get_object_elevation_from_id($object_id);
+                    $elevoffset_old = get_object_offset_from_id($object_id);
+                    $orientation_old = get_object_true_orientation_from_id($object_id);
+
+                    echo "<tr";
+                    if ($object["ob_text"] != $notes)
+                        echo " style=\"border: 2px solid rgb(255, 200, 0)\"";
+                    echo "><td>Description</td><td>".$object["ob_text"]."</td><td>".$notes."</td></tr>\n";
+                    echo "<tr";
+                    if ($lon_old != $lon)
+                        echo " style=\"border: 2px solid rgb(255, 200, 0)\"";
+                    echo "><td>Longitude</td><td>".$lon_old."</td><td>".$lon."</td></tr>\n";
+                    echo "<tr";
+                    if ($lat_old != $lat)
+                        echo " style=\"border: 2px solid rgb(255, 200, 0)\"";
+                    echo "><td>Latitude</td><td>".$lat_old."</td><td>".$lat."</td></tr>\n";
+                    echo "<tr";
+                    if ($elev_old != $elev)
+                        echo " style=\"border: 2px solid rgb(255, 200, 0)\"";
+                    echo "><td>Elevation</td><td>".$elev_old."</td><td>".$elev."</td></tr>\n";
+                    echo "<tr";
+                    if ($elevoffset_old != $elevoffset)
+                        echo " style=\"border: 2px solid rgb(255, 200, 0)\"";
+                    echo "><td>Elevation offset</td><td>".$elevoffset_old."</td><td>".$elevoffset."</td></tr>\n";
+                    echo "<tr";
+                    if ($orientation_old != $orientation)
+                        echo " style=\"border: 2px solid rgb(255, 200, 0)\"";
+                    echo "><td>Heading (STG)</td><td>".heading_true_to_stg($orientation_old)." (STG) - ".$orientation_old."(true)</td><td>".heading_true_to_stg($orientation)." (STG) - ".$orientation." (true)</td></tr>\n";
+                    echo "<tr";
+                    if ($object["ob_text"] != $notes)
+                        echo " style=\"border: 2px solid rgb(255, 200, 0)\"";
+                    echo "><td>Object</td><td>".object_name($object["ob_model"])."</td><td>".object_name($model_id)."</td></tr>\n";
+                    echo "<tr><td>Map</td><td><object data=\"http://mapserver.flightgear.org/popmap/?lon=".$lon_old."&amp;lat=".$lat_old."&amp;zoom=14\" type=\"text/html\" width=\"320\" height=\"240\"></object></td><td><object data=\"http://mapserver.flightgear.org/popmap/?lon=".$lon."&amp;lat=".$lat."&amp;zoom=14\" type=\"text/html\" width=\"320\" height=\"240\"></object></td></tr>\n" .
+                         "</tr>\n";
+?>
+                    <!--<tr>
+                        <td colspan="3">Leave a comment to the submitter</td>
+                        <td colspan="5"><input type="text" name="maintainer_comment" size="85" value="Drop a comment to the submitter" onfocus="emptyDefaultValue(this, 'Drop a comment to the submitter');"/></td>
+                    </tr>-->
+                    <tr>
+                        <td colspan="3" class="submit">                            
+                            <?php echo "<a href=\"update_submission.php?action=accept&amp;sig=".$_GET[sig]."&amp;email=".$_GET[email]."\" />Accept</a> | ";?>
+                            <?php echo "<a href=\"update_submission.php?action=reject&amp;sig=".$_GET[sig]."&amp;email=".$_GET[email]."\" />Reject</a>";?>
+                        </td>
+                    </tr>
+                    </table>
+<?php
+                    include '../../inc/footer.php';
+                }
+            }
+        }
+    }
 
     // Check the presence of "action", the presence of "signature", its length (64) and its content.
     if (isset($_GET["sig"]) && (strlen($_GET["sig"]) == 64) && preg_match("/[0-9a-z]/",$_GET["sig"]) && $_GET["action"] == 'accept') {
