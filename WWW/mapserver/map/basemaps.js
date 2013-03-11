759465bed6f4736542a2921f070259689a3f549c
Martin Spott <Martin.Spott@mgras.net>
2013-03-11 10:16:36 -0700

    Cache Custom Scenery layers

--- WWW/mapserver/map/basemaps.js
+++ WWW/mapserver/map/basemaps.js
@@ -1,17 +1,17 @@
-    var customscene = new OpenLayers.Layer.WMS( "Landcover-DB CS Test",
-        [ "http://2.flightgear.telascience.org/ms?srs=EPSG%3A900913&",
-          "http://4.flightgear.telascience.org/ms?srs=EPSG%3A900913&" ],
-        {layers: 'cs_herbtundra,cs_heath,cs_barrencover,cs_dirt,cs_rainforest,cs_mixedforest,cs_evergreenforest,cs_deciduousforest,cs_olives,cs_orchard,cs_vineyard,cs_burnt,cs_sclerophyllous,cs_scrub,cs_greenspace,cs_grassland,cs_golfcourse,cs_agroforest,cs_cropgrass,cs_naturalcrop,cs_complexcrop,cs_mixedcrop,cs_rice,cs_irrcrop,cs_drycrop,cs_cemetery,cs_transport,cs_construction,cs_industrial,cs_dump,cs_openmining,cs_lava,cs_rock,cs_saltmarsh,cs_littoral,cs_saline,cs_sand,cs_marsh,cs_bog,cs_estuary,cs_lagoon,cs_floodland,cs_town,cs_suburban,cs_port,cs_urban,cs_polarice,cs_packice,cs_glacier,cs_watercourse,cs_intermittentlake,cs_lake,cs_asphalt,cs_airport', format: 'image/png'},
-        {minZoomLevel: 10}
-    );
-
 //    var customscene = new OpenLayers.Layer.WMS( "Landcover-DB CS Test",
-//        [ "http://1.flightgear.telascience.org/tc?",
-//          "http://2.flightgear.telascience.org/tc?",
-//          "http://3.flightgear.telascience.org/tc?",
-//          "http://4.flightgear.telascience.org/tc?" ],
-//        {layers: 'customscene', format: 'image/png'}
+//        [ "http://2.flightgear.telascience.org/ms?srs=EPSG%3A900913&",
+//          "http://4.flightgear.telascience.org/ms?srs=EPSG%3A900913&" ],
+//        {layers: 'cs_herbtundra,cs_heath,cs_barrencover,cs_dirt,cs_rainforest,cs_mixedforest,cs_evergreenforest,cs_deciduousforest,cs_olives,cs_orchard,cs_vineyard,cs_burnt,cs_sclerophyllous,cs_scrub,cs_greenspace,cs_grassland,cs_golfcourse,cs_agroforest,cs_cropgrass,cs_naturalcrop,cs_complexcrop,cs_mixedcrop,cs_rice,cs_irrcrop,cs_drycrop,cs_cemetery,cs_transport,cs_construction,cs_industrial,cs_dump,cs_openmining,cs_lava,cs_rock,cs_saltmarsh,cs_littoral,cs_saline,cs_sand,cs_marsh,cs_bog,cs_estuary,cs_lagoon,cs_floodland,cs_town,cs_suburban,cs_port,cs_urban,cs_polarice,cs_packice,cs_glacier,cs_watercourse,cs_intermittentlake,cs_lake,cs_asphalt,cs_airport', format: 'image/png'},
+//        {minZoomLevel: 10}
 //    );
+
+    var customscene = new OpenLayers.Layer.WMS( "Landcover-DB CS Test",
+        [ "http://1.flightgear.telascience.org/tc?",
+          "http://2.flightgear.telascience.org/tc?",
+          "http://3.flightgear.telascience.org/tc?",
+          "http://4.flightgear.telascience.org/tc?" ],
+        {layers: 'v0cover', format: 'image/png'}
+    );
 
     var v0cover = new OpenLayers.Layer.WMS( "Landcover-DB VMap0 Ed.5",
         [ "http://1.flightgear.telascience.org/tc?",
