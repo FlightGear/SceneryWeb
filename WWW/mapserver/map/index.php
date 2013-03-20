89e5577a50a3f83465c7bca24ad5227948a9d587
Martin Spott <Martin.Spott@mgras.net>
2013-03-19 17:35:29 -0700

    Add airspaces to map

--- WWW/mapserver/map/index.php
+++ WWW/mapserver/map/index.php
@@ -160,13 +160,14 @@
             airfield.setVisibility(true);
             navaid.setVisibility(false);
             fix.setVisibility(false);
+            airspaces.setVisibility(false);
             sceneobject.setVisibility(false);
             gshhs.setVisibility(false);
             fgbuckets.setVisibility(false);
             downloadbox.setVisibility(false);
             opacity_sliders.setVisibility(false);
 
-            map.addLayers([customscene, v0cover, yahoosat, googlesat, bingaerial, icubed, mapnik, bingroad, clc00, clc06, nlcd2006r, clc06v16r, tarmac, tarmac850, osmtarmac, cslines, osmlines, osmlinecover, noaroads, airfield, navaid, fix, sceneobject, gshhs, fgbuckets, downloadbox, opacity_sliders]);
+            map.addLayers([customscene, v0cover, yahoosat, googlesat, bingaerial, icubed, mapnik, bingroad, clc00, clc06, nlcd2006r, clc06v16r, tarmac, tarmac850, osmtarmac, cslines, osmlines, osmlinecover, noaroads, airfield, navaid, fix, airspaces, sceneobject, gshhs, fgbuckets, downloadbox, opacity_sliders]);
 
             // click control
             var click = new OpenLayers.Control.Click();
