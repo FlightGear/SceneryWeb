770fa6aeeccbc9d2294737d48692127d047e878d
Martin Spott <Martin.Spott@mgras.net>
2013-06-17 00:21:30 +0200

    Remove obsolete ballast

--- WWW/mapserver/neworleans.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $location = "-90+28.7+-88.74+29.96";
-  include 'include/defaultlayers.php';
-  include 'include/URL.php';
-  header("Location: $URL");
-?>
--- WWW/mapserver/sandiego.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $location = "-117.491326+32.338554+-116.491326+33.338554";
-  include 'include/defaultlayers.php';
-  include 'include/URL.php';
-  header("Location: $URL");
-?>
