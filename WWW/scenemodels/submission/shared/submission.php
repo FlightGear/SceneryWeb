66272f8e1f7b0d0b20ae7df26122990a3df2919e
OliverJ <acomv41@yahoo.com>
2013-09-14 10:55:00 +0200

    Debugging

--- WWW/scenemodels/submission/shared/submission.php
+++ WWW/scenemodels/submission/shared/submission.php
@@ -41,14 +41,14 @@ if (isset($_GET["action"]) && isset($_GET["sig"]) && (strlen($_GET["sig"]) == 64
 
             // Gzuncompress the query
             $query_rw = gzuncompress($sqlz);
-            echo $query_rw;
+            echo "Query_rw: ".$query_rw;
             $page_title = "Automated Objects Massive Import Requests Form";
             include '../../inc/header.php';
             echo "<p class=\"center\">Signature found.<br /> Now processing query with request number ". $_GET["sig"].".\n</p>\n";
 
             if ($_GET["action"] == "check") {
                 $trigged_query_rw = strstr($query_rw, 'ST_PointFromText'); // Removing the start of the query from the data;
-                echo $trigged_query_rw;
+                echo "\nTrigged query: ".$trigged_query_rw;
                 echo "<table>\n<tr>\n<th>Longitude</th>\n<th>Latitude</th>\n<th>Country</th>\n<th>Elevation</th>\n<th>Elev. offset</th>\n<th>True orientation</th>\n<th>Model</th>\n<th>Map</th>\n</tr>\n";
 
                 $pattern = "/ST_PointFromText\('POINT\((?P<long>[0-9.-]+) (?P<lat>[0-9.-]+)\)', 4326\), (?P<elev>[0-9.-]+), (?P<elevoffset>(([0-9.-]+)|NULL)), (?P<orientation>[0-9.-]+), '(?P<country>[a-z]+)', (?P<model_id>[0-9]+), 1\)/";
