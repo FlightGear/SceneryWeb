0c6bc64a821ee1a56565cfccf21cd3d85a4f0ea1
Julien Nguyen <jnguyen@etu.emse.fr>
2014-04-27 00:04:15 +0200

    Fixing old bug with preg_match

--- WWW/scenemodels/submission/model/static_submission.php
+++ WWW/scenemodels/submission/model/static_submission.php
@@ -294,7 +294,7 @@ if (!isset($_POST["action"])) {
 
             // Retrieve data from query
             $pattern = "/INSERT INTO fgs_models \(mo_id, mo_path, mo_author, mo_name, mo_notes, mo_thumbfile, mo_modelfile, mo_shared\) VALUES \(DEFAULT, '(?P<path>[a-zA-Z0-9_.-]+)', (?P<author>[0-9]+), '(?P<name>[a-zA-Z0-9,;:?@ !_.-]+)', '(?P<notes>[a-zA-Z0-9 ,!_.-]*)', '(?P<thumbfile>[a-zA-Z0-9=+\/]+)', '(?P<modelfile>[a-zA-Z0-9=+\/]+)', (?P<shared>[0-9]+)\) RETURNING mo_id/";
-            $error === preg_match($pattern, $query_rw, $matches);
+            preg_match($pattern, $query_rw, $matches);
 
             $mo_path      = $matches['path'];
             $mo_author    = get_authors_name_from_authors_id($matches['author']);
