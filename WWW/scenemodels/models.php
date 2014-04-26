e3e5419e5920f734de104ce627bf42866ef0c014
Julien Nguyen <jnguyen@etu.emse.fr>
2014-04-26 23:03:24 +0200

    Fixing models.php and objects.php

--- WWW/scenemodels/models.php
+++ WWW/scenemodels/models.php
@@ -47,7 +47,7 @@ $pagesize = 10;
              "<ul class=\"table\">" .
              "<li><b>Name:</b> ".$modelMetadata->getName()."</li>\n" .
              "<li><b>Path:</b> ".$modelMetadata->getFilename()."</li>\n";
-        if ($modelMetadata->getDescription() != NULL && !empty($modelMetadata->getDescription())) {
+        if ($modelMetadata->getDescription() !== NULL && !empty($modelMetadata->getDescription())) {
             echo "<li><b>Notes:</b> ".$modelMetadata->getDescription()."</li>\n";
         }
         echo "<li><b>Author: </b><a href=\"author.php?id=".$modelMetadata->getAuthor()->getId()."\">".$modelMetadata->getAuthor()->getName()."</a></li>\n" .
