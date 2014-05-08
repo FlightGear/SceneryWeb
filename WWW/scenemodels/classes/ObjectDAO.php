baf86099ce16a3a181b6135aea64f689195b6558
Julien Nguyen <jnguyen@etu.emse.fr>
2014-05-08 23:02:06 +0200

    Small optimization

--- WWW/scenemodels/classes/ObjectDAO.php
+++ WWW/scenemodels/classes/ObjectDAO.php
@@ -27,11 +27,13 @@ class ObjectDAO extends PgSqlDAO implements IObjectDAO {
     public function getObject($objectId) {
         $result = $this->database->query("SELECT *, ST_Y(wkb_geometry) AS ob_lat, ST_X(wkb_geometry) AS ob_lon, fn_SceneDir(wkb_geometry) AS ob_dir ".
                                          "FROM fgs_objects, fgs_countries WHERE ob_id=".$objectId." AND ob_country = co_code;");
-        if (pg_num_rows($result) == 0) {
+        
+        $objectRow = pg_fetch_assoc($result);
+        
+        if (!$objectRow) {
             throw new Exception('No object with id '. $objectId. ' was found!');
         }
         
-        $objectRow = pg_fetch_assoc($result);
         return $this->getObjectFromRow($objectRow);
     }
     
