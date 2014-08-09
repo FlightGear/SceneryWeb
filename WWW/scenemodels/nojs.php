b36fa9181f5967e7cf5d2cf1e61cc190fd1bc8ef
Julien Nguyen <jnguyen@etu.emse.fr>
2014-08-09 20:51:48 +0200

    Fix nojs page

--- WWW/scenemodels/nojs.php
+++ WWW/scenemodels/nojs.php
@@ -1,6 +1,9 @@
 <?php
 $nojs_page = true;
-require 'inc/header.php';?>
+require_once 'classes/DAOFactory.php';
+require 'inc/header.php';
+$modelDaoRO = DAOFactory::getInstance()->getModelDaoRO();
+?>
 <h1>Your Javascript is disabled</h1>
 
 <p>For better navigation, please activate Javascript!</p>
