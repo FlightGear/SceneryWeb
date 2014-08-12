aca50a0b8f98d0e2834e2364a5b0b50d1b28e27e
Julien Nguyen <jnguyen@etu.emse.fr>
2014-08-12 23:42:45 +0200

    Debugging update submission page

--- WWW/scenemodels/classes/RequestDAO.php
+++ WWW/scenemodels/classes/RequestDAO.php
@@ -395,7 +395,7 @@ class RequestDAO extends PgSqlDAO implements IRequestDAO {
         // Removing the start of the query from the data
         $triggedQuery = strstr($requestQuery, 'SET');
         $triggedQuery = str_replace('$','',$triggedQuery);
-
+        echo $triggedQuery;
         $pattern = "/SET ob_text\=(?P<notes>[a-zA-Z0-9 +,!_.;\(\)\[\]\/-]*), wkb_geometry\=ST_PointFromText\('POINT\((?P<lon>[0-9.-]+) (?P<lat>[0-9.-]+)\)', 4326\), ob_gndelev\=(?P<elev>[0-9.-]+), ob_elevoffset\=(?P<elevoffset>(([0-9.-]+)|NULL)), ob_heading\=(?P<orientation>[0-9.-]+), ob_model\=(?P<model_id>[0-9]+), ob_group\=1 WHERE ob_id\=(?P<object_id>[0-9]+)/";
 
         preg_match($pattern, $triggedQuery, $matches);
