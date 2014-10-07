23ec680a8578a364231ad723177cd1e9fa345011
Martin Spott <Martin.Spott@mgras.net>
2014-10-07 16:52:31 -0700

    Replace old cruft in template view by functional layers

--- WWW/mapserver/include/defaultlayers.php
+++ WWW/mapserver/include/defaultlayers.php
@@ -1,3 +1,3 @@
 <?php
-  $layers = "layer=v0_stream&layer=v0_intermittentstream&layer=v0_freeway&layer=v0_road&layer=v0_lake&layer=v0_urban&layer=gshhs_coastline&layer=apt_airfield&layer=apt_runway&layer=apt_tarmac"
+  $layers = "layer=osm_river&layer=osm_motorway&layer=osm_primary&layer=cs_lake&layer=cs_urban&layer=osm_coastline&layer=apt_airfield&layer=apt_runway&layer=apt_tarmac"
 ?>
