b944856c5e78aa12f2882113cbc38547d637b3b6
Julien Nguyen <jnguyen@etu.emse.fr>
2014-10-12 18:01:50 +0200

    Testing that all is ok

--- WWW/scenemodels/classes/RequestDAO.php
+++ WWW/scenemodels/classes/RequestDAO.php
@@ -287,7 +287,6 @@ class RequestDAO extends PgSqlDAO implements IRequestDAO {
     }
     
     private function getRequestModelAddFromRow($requestQuery) {
-	echo $requestQuery;
         $queryModel = substr($requestQuery, 0, strpos($requestQuery, ";INSERT INTO fgs_objects"));
         $queryObj = strstr($requestQuery, "INSERT INTO fgs_objects");
 
