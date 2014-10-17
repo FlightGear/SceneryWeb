bffe6a5e1f7d00f79c2a9085cc6a2b38eacb662d
Martin Spott <Martin.Spott@mgras.net>
2014-10-17 09:41:48 -0700

    Something entirely new: Add comments to SQL code  ;-)

--- SQL/v_apt_heading.sql
+++ SQL/v_apt_heading.sql
@@ -18,24 +18,34 @@
 
 BEGIN TRANSACTION;
     DROP MATERIALIZED VIEW v_apt_heading;
+    -- This resembles a temporary table.
     CREATE MATERIALIZED VIEW v_apt_heading AS (
         WITH h AS (
             SELECT icao,
+                -- Strip trailing non-numeric characters from runway designator.
                 (CASE WHEN COALESCE(substring(rwy_num1 FROM '[0-9]*'), rwy_num1) != ''
                     THEN COALESCE(substring(rwy_num1 FROM '[0-9]*'), rwy_num1)
                     ELSE rwy_num1
+                    -- Convert rnuway designation string into number as degrees.
                     END)::numeric * 10 AS heading,
+                -- obsolete ....
                 COUNT(*),
-                ROW_NUMBER() OVER (PARTITION BY icao ORDER BY icao, COUNT(*) DESC) AS rnk
+                -- Rank rnuway headings by number of occurrences.
+                ROW_NUMBER() OVER (PARTITION BY icao ORDER BY icao, COUNT(*) DESC) AS rank
             FROM apt_runway
+            -- FIXME, skip heliports, they start with a non-numeric character.
             WHERE left(rwy_num1, 1) != 'H'
             GROUP BY icao, heading)
 
+        -- Views don't have serials, create a work-alike.
         SELECT ROW_NUMBER() OVER (ORDER BY a.icao) AS ogc_fid,
+            -- Join weighted airport center with heading ....
             a.icao AS icao, ST_Centroid(ST_Collect(a.wkb_geometry)) AS wkb_geometry, h.heading
         FROM apt_runway AS a, h
-        WHERE a.icao = h.icao AND h.rnk = 1
+        -- .... which has the highest rank.
+        WHERE a.icao = h.icao AND h.rank = 1
         GROUP BY a.icao, h.heading
         ORDER BY icao);
+    -- Let everybody read the view.
     GRANT SELECT ON v_apt_heading TO webuser;
 COMMIT;
