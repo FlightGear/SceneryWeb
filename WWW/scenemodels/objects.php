8fb19cb7bcc7d58d19d1cb77e3ce95ec617d165e
Julien Nguyen <julien.nguyen3@gmail.com>
2014-11-21 22:04:21 +0100

    Improve HTML

--- WWW/scenemodels/objects.php
+++ WWW/scenemodels/objects.php
@@ -211,7 +211,7 @@ if (isset($_REQUEST['description']) && preg_match('/^[A-Za-z0-9 \-\.\,]+$/u',$_R
                  "  <td>".$object->getGroundElevation()."<br/>".$offset."</td>\n" .
                  "  <td>".$object->getOrientation()."</td>\n" .
                  "  <td style=\"width: 58px; text-align: center\">\n" .
-                 "  <a href=\"submission/object/check_update.php?id_to_update=".$object->getId()."\"><img class=\"icon\" src=\"http://scenery.flightgear.org/img/icons/edit.png\"/></a>";
+                 "  <a href=\"submission/object/check_update.php?id_to_update=".$object->getId()."\"><img class=\"icon\" src=\"http://scenery.flightgear.org/img/icons/edit.png\" alt=\"edit\"/></a>";
             if ($modelDAO->getModelMetadata($object->getModelId())->getModelsGroup()->isStatic()) {
 ?>
                 <a href="submission/object/check_delete_shared.php?delete_choice=<?php echo $object->getId(); ?>">
@@ -219,7 +219,7 @@ if (isset($_REQUEST['description']) && preg_match('/^[A-Za-z0-9 \-\.\,]+$/u',$_R
                 </a>
 <?php
             }
-            echo "    <a href=\"javascript:popmap(".$object->getLatitude().",".$object->getLongitude().")\"><img class=\"icon\" src=\"http://scenery.flightgear.org/img/icons/world.png\"/></a>" .
+            echo "    <a href=\"javascript:popmap(".$object->getLatitude().",".$object->getLongitude().")\"><img class=\"icon\" src=\"http://scenery.flightgear.org/img/icons/world.png\" alt=\"map\"/></a>" .
                  "  </td>\n" .
                  "</tr>\n";
         }
