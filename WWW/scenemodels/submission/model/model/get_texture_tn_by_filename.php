ec85243337bbe9f9d12b4f4c3fa8ca0aa288834e
Julien Nguyen <julien.nguyen3@gmail.com>
2014-12-28 23:53:36 +0100

    Simplify thumbnail texture

--- WWW/scenemodels/submission/model/model/get_texture_tn_by_filename.php
+++ WWW/scenemodels/submission/model/model/get_texture_tn_by_filename.php
@@ -4,7 +4,6 @@ $dir_array = preg_split("/\//", $_GET['name']);
 $filenameText = $dir_array[count($dir_array)-1];
 
 // Inserting libs
-require_once '../../../inc/functions.inc.php';
 require_once '../../../autoload.php';
 $requestDaoRO = DAOFactory::getInstance()->getrequestDaoRO();
 
@@ -19,25 +18,9 @@ try {
     exit;
 }
 
-$modelfiles = $request->getNewModel()->getModelFiles();
+$textureContent = $request->getNewModel()->getModelFiles()->getTexture($filenameText);
 
-// Prepare the tmp directory
-
-// Managing possible concurrent accesses on the maintainer side.
-$target_path = open_tgz($modelfiles->getPackage());
-
-// Looking for the file in the tmp directory
-$dir = opendir($target_path);
-
-while ($filename = readdir($dir)) {
-    // If we know the extension
-    if (show_file_extension($filename) == "png" && $filename == $filenameText) {
-        $filepath = $target_path."/".$filename;
-        break;
-    }
-}
-
-$img = imagecreatefrompng( $filepath );
+$img = imagecreatefromstring($textureContent);
 $width = imagesx( $img );
 $height = imagesy( $img );
 
@@ -60,10 +43,6 @@ if ($width>256) {
     imagedestroy($tmp_img);
 }
 else {
-    readfile($filepath);
+    echo $textureContent;
 }
-
-// Ok, now we can delete the stuff we used - at least I think so ;-)
-// This should be done at the end of the script
-close_tgz($target_path);
 ?>
