190bc248eb7bb6b10b2a15bee8672865b003111a
Julien Nguyen <julien.nguyen3@gmail.com>
2014-12-29 22:42:40 +0100

    Merge two files

--- WWW/scenemodels/submission/model/model/inc_getfile.php
+++ WWW/scenemodels/submission/model/model/inc_getfile.php
@@ -3,10 +3,36 @@
  * This file is used to retrieve a file from tgz models
  *
  * To use this script, first define;
- * $mo_sig: containing the value to define the directory
- * $extension : file extension OR $filename (if there can be more than 2 with same extension for instance)
- *
+ * mo_sig: containing the value to define the directory
+ * type : file type
+ * name (optional) : filename
 **/
+
+function displayThumbnail($textureContent) {
+    $img = imagecreatefromstring($textureContent);
+    
+    $width = imagesx( $img );
+    $height = imagesy( $img );
+    
+    if ($width>256) {
+        // calculate thumbnail size
+        $new_width = 256;
+        $new_height = floor( $height * $new_width / $width );
+
+        // create a new temporary image
+        $tmp_img = imagecreatetruecolor( $new_width, $new_height );
+
+        // copy and resize old image into new image 
+        imagecopyresized( $tmp_img, $img, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
+
+        // Display the PNG directly to the browser
+        imagepng($tmp_img);
+        imagedestroy($tmp_img);
+    }
+    else {
+        echo $textureContent;
+    }
+}
 
 // Inserting libs
 require_once '../../../autoload.php';
@@ -15,9 +41,17 @@ $requestDaoRO = DAOFactory::getInstance()->getrequestDaoRO();
 $mo_sig = $_GET["mo_sig"];
 $type = $_REQUEST['type'];
 
-if ((isset($filename) && !FormChecker::isFilename($filename))
-        || !FormChecker::isSig($mo_sig)) {
+if (!FormChecker::isSig($mo_sig)) {
     exit;
+}
+
+if (!empty($_GET['name'])) {
+    $dir_array = preg_split("/\//", $_GET['name']);
+    $filename = $dir_array[count($dir_array)-1];
+    
+    if (!FormChecker::isFilename($filename)) {
+        exit;
+    }
 }
 
 try {
@@ -39,11 +73,14 @@ switch ($type) {
         echo $modelfiles->getACFile();
         break;
     case "texture":
-        $dir_array = preg_split("/\//", $_GET['name']);
-        $filename = $dir_array[count($dir_array)-1];
-        
         header("Content-type: image/png");
         echo $modelfiles->getTexture($filename);
+        break;
+    case "thumbtexture":
+        header('Content-Type: image/png');
+        displayThumbnail($modelfiles->getTexture($filename));
+        break;
+    default:
         break;
 }
 
