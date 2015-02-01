409507332539adfdb7f3cda3a2fa50f1a1cd3cf1
Julien Nguyen <julien.nguyen3@gmail.com>
2015-02-01 13:03:04 +0100

    Simplify get extension function

--- WWW/scenemodels/inc/functions.inc.php
+++ WWW/scenemodels/inc/functions.inc.php
@@ -58,15 +58,7 @@ function heading_true_to_stg($true_heading) {
 // =================================================
 
 function show_file_extension($filepath) {
-    preg_match('/[^?]*/', $filepath, $matches);
-    $string = $matches[0];
-    $pattern = preg_split('/\./', $string, -1, PREG_SPLIT_OFFSET_CAPTURE);
-
-    if (count($pattern) > 1) {
-        $filenamepart = $pattern[count($pattern)-1][0];
-        preg_match('/[^?]*/', $filenamepart, $matches);
-        return $matches[0];
-    }
+    return pathinfo($filepath, PATHINFO_EXTENSION);
 }
 
 // Deletes a directory sent in parameter
