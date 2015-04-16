3ce2fe34e456d0fc9312d4150ea68ec220e7e9eb
Martin Spott <Martin.Spott@mgras.net>
2015-04-16 02:58:16 -0700

    Apparently nobody noticed this was gone/broken  :-)

--- WWW/mapserver/map/basics.php
+++ WWW/mapserver/map/basics.php
@@ -1,5 +1,5 @@
-<!--    <link rel="stylesheet" href="http://www.openlayers.org/dev/theme/default/style.css" type="text/css" />
-    <link rel="stylesheet" href="http://www.openlayers.org/dev/examples/style.css" type="text/css" /> -->
+<!--    <link rel="stylesheet" href="http://www.openlayers.org/dev/theme/default/style.css" type="text/css" /> -->
+    <link rel="stylesheet" href="http://dev.openlayers.org/examples/style.css" type="text/css" />
     <script src="http://dev.openlayers.org/OpenLayers.js"></script>
 <!--    <script type="text/javascript" src="/ol/OpenLayers.js"></script> -->
 <!--    <script src="../openlayers-ys/OpenLayers.js"></script> -->
