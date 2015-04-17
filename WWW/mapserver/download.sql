8a46f12f2c2234528e0582a79057ad2deed863a4
Martin Spott <Martin.Spott@mgras.net>
2015-04-17 15:29:24 -0700

    Column rename.

--- WWW/mapserver/download.sql
+++ WWW/mapserver/download.sql
@@ -1,10 +1,11 @@
 DROP TABLE download;
 CREATE TABLE download (
     uuid varchar NOT NULL,
-    ll_geometry geometry NOT NULL,
-    ur_geometry geometry NOT NULL,
-    pgislayer varchar NOT NULL,
-    requestdate timestamp NOT NULL
+    wkb_geometry geometry NOT NULL,
+    feature varchar NOT NULL,
+    date timestamp NOT NULL,
+    ip inet DEFAULT NULL,
+    single boolean NOT NULL DEFAULT TRUE
 ) WITH OIDS;
 ALTER TABLE download ADD PRIMARY KEY (id);
 ALTER TABLE download CLUSTER ON download_pkey;
