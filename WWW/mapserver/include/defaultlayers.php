5a77a6fe38ed5a7e6d3d7d16b82903e97d6d3cb6
Martin Spott <Martin.Spott@mgras.net>
2015-04-19 14:14:50 -0700

    SVG symbols non-functional in template view.

--- WWW/mapserver/include/defaultlayers.php
+++ WWW/mapserver/include/defaultlayers.php
@@ -1,3 +1,3 @@
 <?php
-  $layers = "layer=osm_stream&layer=osm_river&layer=osm_motorway&layer=osm_primary&layer=osm_secondary&layer=cs_lake&layer=cs_urban&layer=osm_coastline&layer=apt_airfield&layer=apt_runway&layer=apt_tarmac"
+  $layers = "layer=osm_stream&layer=osm_river&layer=osm_motorway&layer=osm_primary&layer=osm_secondary&layer=cs_lake&layer=cs_urban&layer=osm_coastline&layer=apt_runway&layer=apt_tarmac"
 ?>
