f2bd4b7ff8ec9f7a9c3f9bf76a3d7854ca31f1e4
Martin Spott <Martin.Spott@mgras.net>
2015-06-16 10:09:20 -0700

    X-Plane are extending ICAO to 5 digits ....

--- WWW/mapserver/include/icaocheck.php
+++ WWW/mapserver/include/icaocheck.php
@@ -4,8 +4,8 @@
   {
     die ("Please enter valid ICAO code");
   }
-  # Limit ICAO strings to 4 characters
-  if(strlen($icao) > 4)
+  # Limit ICAO strings to 5 characters
+  if(strlen($icao) > 5)
   {
     die ("ICAO code too long !");
   }
