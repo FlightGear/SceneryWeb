9886e12f599d2f00bb741c4018ca67f031280da2
Martin Spott <Martin.Spott@mgras.net>
2015-08-11 08:35:54 -0700

    Add Copyright notices.

--- WWW/mapserver/dlsect.sh
+++ WWW/mapserver/dlsect.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 #
+# Copyright (C) 2012 - 2014 Martin Spott - Martin (at) flightgear (dot) org
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 # Download sector data for OpenRADAR; maprange x="3.0" y="3.0"
 #
 # Supply bounding box for 'pgsql2shp' as:
 #   <xmin> <ymin>,<xmax> <ymax>
+#
 
 UUID=${1}
 PGHOST=localhost
