a99aa972eb58528aea984eb807d9b55b30511966
Torsten Dreyer <torsten@t3r.de>
2015-12-24 11:20:56 +0100

    Adjust footer.php to new environment
    - new location of .git
    - no webalizer (yet)

--- WWW/scenemodels/view/footer.php
+++ WWW/scenemodels/view/footer.php
@@ -4,17 +4,19 @@
 <?php
         // What's the last GIT version of the website and when was it last
         // updated?
-        $filename = '/home/fgscenery/GIT/.git/refs/heads/master';
+        $filename = '/srv/sceneryweb/.git/refs/heads/master';
         if (file_exists($filename)) {
             $result = file_get_contents($filename);
             echo "Version&nbsp;".substr($result,0,7)."&nbsp;-&nbsp;" .
                  date("F d Y H:i", filemtime($filename)) ."&nbsp;-&nbsp;";
         }
 ?>
-        <a href="http://mapserver.flightgear.org/git/gitweb.pl?p=sceneryweb">Version info</a> - 
+        <a href="https://sourceforge.net/p/flightgear/sceneryweb/commit_browser">Version info</a> - 
         <a href="../../TOBEDONE">Volunteer?</a> - <a href="../../README">Readme</a> - 
         <a href="../../LICENCE">License</a> - <a href="../../VERSION">History</a> - 
+<!--
         <a href="http://sphere.telascience.org/webalizer/">Web statistics</a>
+-->
     </p>
 
 </body>
