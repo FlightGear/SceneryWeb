662086e06c9f46efa07a8a8bfeb8f912132d8ef7
Torsten Dreyer <torsten@t3r.de>
2016-01-26 12:30:04 +0100

    fine tune the packing script for sf

--- Export/pack.sh
+++ Export/pack.sh
@@ -6,13 +6,13 @@ mkdir -p "$OUTPUTDIR"
 
 tar --create --owner=root --group=root --gzip --file "$OUTPUTDIR/SharedModels.tgz" Models
 tar --create --owner=root --group=root --gzip --file "$OUTPUTDIR/GlobalObjects.tgz" Objects
-for f in Objects/[ew][0-9][0-9]0[ns][0-9]0; do
+for f in Objects/[ew][01][0-9]0[ns][0-9]0; do
   tar --create --owner=root --group=root --gzip --file "$OUTPUTDIR/$(basename $f).tgz" "$f"
 done
 
 cd "$OUTPUTDIR"
 
-rsync -av --checksum [ew][1][0-9][0-9][ns][0-9][0-1].tgz torstendreyer,flightgear@web.sourceforge.net:htdocs/scenery/
+rsync -av --checksum [ew][01][0-9]0[ns][0-9]0.tgz torstendreyer,flightgear@web.sourceforge.net:htdocs/scenery/
 rsync -av --checksum GlobalObjects.tgz SharedModels.tgz torstendreyer,flightgear@web.sourceforge.net:/home/frs/project/flightgear/scenery/
 
 popd
