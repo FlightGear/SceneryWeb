5113e119f7818a98ee40d28ba1fc5389981e1c94
Julien Nguyen <julien.nguyen3@gmail.com>
2016-05-21 21:26:46 +0800

    Fix update model page

--- WWW/scenemodels/view/submission/model/validator/view_upd_model_request.php
+++ WWW/scenemodels/view/submission/model/validator/view_upd_model_request.php
@@ -144,10 +144,10 @@ include 'view/header.php';
 <?php
             $texturesNames = $oldModel->getModelFiles()->getTexturesNames();
             foreach ($texturesNames as $textureName) {
-                $texture_file = "http://".$_SERVER['SERVER_NAME'] ."/app.php?c=UpdateModelValidator&a=getOldModelTexture&sig=".$sig."&name=".$textureName;
-                $texture_file_tn = "http://".$_SERVER['SERVER_NAME'] ."/app.php?c=UpdateModelValidator&a=getOldModelTexture&sig=".$sig."&name=".$textureName;
+                $texture_file = "/app.php?c=UpdateModelValidator&a=getOldModelTexture&sig=".$sig."&name=".$textureName;
+                $texture_file_tn = "/app.php?c=UpdateModelValidator&a=getOldModelTexture&sig=".$sig."&name=".$textureName;
 
-                $tmp = getimagesize($texture_file);
+                $tmp = $oldModel->getModelFiles()->getFileImageInfos($textureName);
                 $width  = $tmp[0];
                 $height = $tmp[1];
 ?>
@@ -166,7 +166,7 @@ include 'view/header.php';
                 $texture_file = "/app.php?c=UpdateModelValidator&a=getNewModelTexture&sig=".$sig."&name=".$textureName;
                 $texture_file_tn = "/app.php?c=UpdateModelValidator&a=getNewModelTextureTN&sig=".$sig."&name=".$textureName;
 
-                $tmp = $modelFiles->getFileImageInfos($textureName);
+                $tmp = $newModelFiles->getFileImageInfos($textureName);
                 $width  = $tmp[0];
                 $height = $tmp[1];
 ?>
