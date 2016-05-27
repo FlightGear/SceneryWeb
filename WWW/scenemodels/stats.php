27786e97f767801516b98348ed35d9e744fd0764
Torsten Dreyer <torsten@t3r.de>
2016-05-27 11:46:03 +0200

    update latest stats
    should be dynamically created :-/

--- WWW/scenemodels/stats.php
+++ WWW/scenemodels/stats.php
@@ -339,6 +339,7 @@ require 'view/header.php';
             [new Date(2014,6,4), 1176580, 3892, 2310 ],
             [new Date(2014,10,28), 1178742, 4128, 2310 ],
             [new Date(2016,03,12), 1220822, 5190, 2383],
+            [new Date(2016,05,27), 1224239, 5361, 2383],
             [new Date(<?php echo date('Y').",".(date('n')-1).",".date('j')."), ".$objects.", ".$models.", ".$signs; ?> ]
         ]);
 
