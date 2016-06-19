19f8a87cb389d5ee80cdac3b43a9ba8c84b99dbb
Julien Nguyen <julien.nguyen3@gmail.com>
2016-06-19 23:41:41 +0800

    Use https links in RSS

--- WWW/scenemodels/rss/news.php
+++ WWW/scenemodels/rss/news.php
@@ -13,7 +13,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 <rss version="2.0">
   <channel>
     <title>FGFSDB Updates</title>
-    <link>http://<?php echo $_SERVER['SERVER_NAME'];?></link>
+    <link>https://<?php echo $_SERVER['SERVER_NAME'];?></link>
     <language>en-GB</language>
     <copyright>Jon Stockill 2006.</copyright>
     <description>FlightGear scenery object database updates.</description>
@@ -26,7 +26,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
     ?>
     <item>
       <link>
-        http://<?php echo $_SERVER['SERVER_NAME'];?>/newsarticle.php?article=<?php echo $newsPost->getId();?>
+        https://<?php echo $_SERVER['SERVER_NAME'];?>/newsarticle.php?article=<?php echo $newsPost->getId();?>
       </link>
       <description><![CDATA[<?php echo $newsPost->getText();?> ]]></description> 
       <pubDate>
